05f0d119378ce4c30607787212e58a3dedb33709
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2013-10-08 16:38:56 +0000

    -- minor refactoring of octif.cc to help with unplanned merge.
    mwm

--- src/octif.cc
+++ src/octif.cc
@@ -32,34 +32,27 @@ namespace boda
     octave_value_list in;
     in(0) = octave_value( mat_fn );
     octave_value_list out = feval ("load", in, 1);
-    if (!error_state && out.length () > 0)
+    assert_st( !error_state && (out.length() > 0) );
+    //cout << "load of ["  << in(0).string_value () << "] is "; out(0).print(cout); cout << endl;
+    //cout << "load ret="; out(0).print(cout); cout << endl;
+    octave_scalar_map osm = out(0).scalar_map_value();
+    assert_st( !error_state );
+    octave_value mod = osm.contents("csc_model");
+    assert_st( !error_state && mod.is_defined() );
+    octave_scalar_map mod_osm = mod.scalar_map_value();
+    assert_st( !error_state );
+    cout << "keys=";
+    for (int i = 0; i < mod_osm.keys().length(); ++i)
     {
-      //cout << "load of ["  << in(0).string_value () << "] is "; out(0).print(cout); cout << endl;
-      //cout << "load ret="; out(0).print(cout); cout << endl;
-      octave_scalar_map osm = out(0).scalar_map_value();
-      assert_st( !error_state );
-      octave_value mod = osm.contents("csc_model");
-      assert_st( !error_state && mod.is_defined() );
-      octave_scalar_map mod_osm = mod.scalar_map_value();
-      assert_st( !error_state );
-      cout << "keys=";
-      for (int i = 0; i < mod_osm.keys().length(); ++i)
-      {
-	cout << mod_osm.keys()[i] << " ";
-      }
-      cout << endl;
-      print_field( mod_osm, "thresh" );
-      print_field( mod_osm, "sbin" );
-      print_field( mod_osm, "interval" );
+      cout << mod_osm.keys()[i] << " ";
+    }
+    cout << endl;
+    print_field( mod_osm, "thresh" );
+    print_field( mod_osm, "sbin" );
+    print_field( mod_osm, "interval" );
 #if 1
-      mxArray mxa( mod );
-      Model mod2( &mxa );
+    mxArray mxa( mod );
+    Model mod2( &mxa );
 #endif
-      
-    }
-    else
-    {
-      std::cout << "invalid\n";
-    }
   }
 }
