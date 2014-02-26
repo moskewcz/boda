e31186d4da1504cd7fbcd94ba5f587f5eb76e691
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2014-02-26 01:12:26 +0000

    -- reorganization and improvments to downsampling code. experimented with openmp for various loops, but left it disabled in the end.
    mwm

--- src/img_io.cc
+++ src/img_io.cc
@@ -7,6 +7,7 @@
 #include<boost/iostreams/device/mapped_file.hpp>
 #include"has_main.H"
 #include"timers.H"
+#include<omp.h>
 
 namespace boda 
 {
@@ -34,6 +35,8 @@ namespace boda
     uint32_t row_size =  depth * w; // 'unaligned' (maybe not mult of row_align) / raw / minimum
     uint32_t const ciel_row_size_over_row_align = (row_size+row_align-1)/row_align;
     row_pitch = ciel_row_size_over_row_align * row_align; // multiple of row_align / padded
+    row_pitch_pels = row_pitch / depth;
+    assert_st( row_pitch_pels * depth == row_pitch ); // could relax?
     pels = ma_p_uint8_t( row_pitch*h, row_align );
   }
 
@@ -88,23 +91,36 @@ namespace boda
     lodepng::save_file(png_file_data, fn);
   }
 
+  uint8_t get_chan( uint8_t const c, uint32_t const v ) { return v >> (c*8); }
+  uint64_t get_chan_64( uint8_t const c, uint32_t const v ) { return get_chan( c, v ); }
+
   p_img_t downsample_w_transpose_2x( img_t const * const src )
   {
     p_img_t ret( new img_t );
     ret->set_row_align( src->row_align ); // preserve alignment
     ret->set_sz_and_alloc_pels( src->h, src->w >> 1 ); // downscale in w and transpose. 
     // note: if src->w is odd, we will drop the last input column
-    for( uint32_t rx = 0; rx < ret->w; ++rx ) {
-      for( uint32_t ry = 0; ry < ret->h; ++ry ) {
+    uint64_t const * src_data = (uint64_t const *) src->pels.get(); 
+    // FIXME/NOTE: when tested, enabling openmp here yielded a 4x
+    // slowdown for this function when openmp was *not* enabled in the
+    // non-2x downsample function, or *no effect* if openmp *was
+    // enabled* in the other downsample function. that doesn't seem to
+    // make sense.
+//#pragma omp parallel for 
+    for( uint32_t ry = 0; ry < ret->h; ++ry ) {
+      uint32_t const sx = ry << 1;
+      for( uint32_t rx = 0; rx < ret->w; ++rx ) {
+	uint32_t const sy = rx;
+	uint64_t const src_data_1 = src_data[ (sy*src->row_pitch_pels + sx)>>1 ];
+	uint32_t const src_data_2 = src_data_1 >> 32;
+	uint32_t dest_val = 0xffu << (3*8); // max alpha
 	for( uint32_t c = 0; c < 3; ++c ) {
-	  uint32_t const sx = ry << 1;
-	  uint32_t const sy = rx;
-	  ret->pels.get()[ ry*ret->row_pitch + rx*ret->depth + c ] =
-//	    src->pels.get()[ sy*src->row_pitch + sx*src->depth + c ];
-	    ( uint16_t( src->pels.get()[ sy*src->row_pitch + sx*src->depth + c ] ) + 
-	      src->pels.get()[ sy*src->row_pitch + (sx+1)*src->depth + c ] + 1 ) >> 1; // note: we round .5 up
+	  dest_val += // note: we round .5 up
+	    uint32_t( uint8_t( (
+				 uint16_t( get_chan(c,src_data_1) ) +
+				 uint16_t( get_chan(c,src_data_2) ) + 1 ) >> 1 ) ) << (c*8);
 	}
-	ret->pels.get()[ ry*ret->row_pitch + rx*ret->depth + 3 ] = uint8_t_const_max; // alpha
+	((uint32_t *)ret->pels.get())[ ry*ret->row_pitch_pels + rx ] = dest_val;
       }
     }
     return ret;
@@ -134,6 +150,7 @@ namespace boda
     return downsample_w_transpose_2x( tmp_img.get() );
   }
 
+
 //#define RESIZE_DEBUG
   // for all downsample functions, scale is 0.16 fixed point, value must be in [.5,1)
   p_img_t downsample_w_transpose( img_t const * const src, uint32_t ds_w )
@@ -155,37 +172,40 @@ namespace boda
     // the input and output image sizes).
     uint64_t const max_src_x = uint64_t(src->w)<<30; 
     //printf( "scale=%s inv_scale=%s ((double)scale/double(1<<16))=%s ((double)inv_scale/double(1<<30))=%s\n", str(scale).c_str(), str(inv_scale).c_str(), str(((double)scale/double(1<<16))).c_str(), str(((double)inv_scale/double(1<<30))).c_str() );
-    for( uint32_t rx = 0; rx < ret->w; ++rx ) {
-      //printf( "********** rx=%s ***********\n", str(rx).c_str() );
-      for( uint32_t ry = 0; ry < ret->h; ++ry ) {
-	for( uint32_t c = 0; c < 3; ++c ) {
-	  uint64_t const sx1_fp = (uint64_t( ry ) * inv_scale); // img_sz_bits.30 fp
-	  uint32_t const sx1 = sx1_fp >> 30;
-	  uint64_t const sx2_fp = std::min( max_src_x, sx1_fp + inv_scale );
-	  uint32_t const sx2 = sx2_fp >> 30;
-	  uint64_t const sx1_w = ( 1U << 30 ) - (sx1_fp - (uint64_t(sx1)<<30)); // 1.30 fp, value (0,1]
-	  uint64_t const sx2_w = sx2_fp - (uint64_t(sx2)<<30); // 1.30 fp, value (0,1]
+    uint32_t const * src_data = (uint32_t const *) src->pels.get(); 
+    assert( src->depth == 4 );
+//#pragma omp parallel for
+    for( uint32_t ry = 0; ry < ret->h; ++ry ) {
+      uint64_t const sx1_fp = (uint64_t( ry ) * inv_scale); // img_sz_bits.30 fp
+      uint32_t const sx1 = sx1_fp >> 30;
+      uint64_t const sx2_fp = std::min( max_src_x, sx1_fp + inv_scale );
+      uint32_t const sx2 = sx2_fp >> 30;
+      uint64_t const sx1_w = ( 1U << 30 ) - (sx1_fp - (uint64_t(sx1)<<30)); // 1.30 fp, value (0,1]
+      uint64_t const sx2_w = sx2_fp - (uint64_t(sx2)<<30); // 1.30 fp, value (0,1]
+      uint32_t const span = sx2 - sx1;
+      assert( (span == 1) || (span == 2) );
 #ifdef RESIZE_DEBUG
-	  printf("------\n");
-	  printf( "ry=%s sx1=%s sx2=%s\n", str(ry).c_str(), str(sx1).c_str(), str(sx2).c_str() );
- 	  printf( "sx1=%s sx2=%s sx1_w=%s sx2_w=%s\n", str(sx1).c_str(), str(sx2).c_str(), str(sx1_w).c_str(), str(sx2_w).c_str() );
-	  printf( "(double(sx1_fp)/double(1<<30))=%s\n", str((double(sx1_fp)/double(1U<<30))).c_str() );
-	  printf( "(double(sx2_fp)/double(1<<30))=%s\n", str((double(sx2_fp)/double(1U<<30))).c_str() );
-
-	  printf( "(double(sx1_w)/double(1<<30))=%s\n", str((double(sx1_w)/double(1U<<30))).c_str() );
-	  printf( "(double(sx2_w)/double(1<<30))=%s\n", str((double(sx2_w)/double(1U<<30))).c_str() );
-#endif
-	  uint32_t const span = sx2 - sx1;
-	  assert( (span == 1) || (span == 2) );
-	  uint64_t const mid_pel = (span==1)?0:( 
-	    uint64_t( src->pels.get()[ rx*src->row_pitch + (sx1+1)*src->depth + c ] ) << 30 );
-//	  printf( "span=%s mid_pel=%s\n", str(span).c_str(), str(mid_pel).c_str() );
-	  ret->pels.get()[ ry*ret->row_pitch + rx*ret->depth + c ] = 
-	    ( (
-	      ( uint64_t( src->pels.get()[ rx*src->row_pitch + sx1*src->depth + c ] ) * sx1_w ) +
-	      ( mid_pel ) +
-	      ( sx2_w ? ( uint64_t( src->pels.get()[ rx*src->row_pitch + sx2*src->depth + c ] ) * sx2_w ) : uint64_t(0) )
-	       ) * scale + (1lu << 45) ) >> 46;
+      printf("------\n");
+      printf( "ry=%s sx1=%s sx2=%s\n", str(ry).c_str(), str(sx1).c_str(), str(sx2).c_str() );
+      printf( "sx1=%s sx2=%s sx1_w=%s sx2_w=%s\n", str(sx1).c_str(), str(sx2).c_str(), str(sx1_w).c_str(), str(sx2_w).c_str() );
+      printf( "(double(sx1_fp)/double(1<<30))=%s\n", str((double(sx1_fp)/double(1U<<30))).c_str() );
+      printf( "(double(sx2_fp)/double(1<<30))=%s\n", str((double(sx2_fp)/double(1U<<30))).c_str() );
+      
+      printf( "(double(sx1_w)/double(1<<30))=%s\n", str((double(sx1_w)/double(1U<<30))).c_str() );
+      printf( "(double(sx2_w)/double(1<<30))=%s\n", str((double(sx2_w)/double(1U<<30))).c_str() );
+#endif	
+      for( uint32_t rx = 0; rx < ret->w; ++rx ) {
+	uint32_t const src_data_1 = src_data[ rx*src->row_pitch_pels + sx1 ];
+	uint32_t const src_data_m = (span==1)?0:src_data[ rx*src->row_pitch_pels + (sx1+1) ];
+	uint32_t const src_data_2 = src_data[ rx*src->row_pitch_pels + sx2 ];
+	uint32_t dest_val = 0xffu << (3*8); // max alpha
+	for( uint32_t c = 0; c < 3; ++c ) {
+	  dest_val += 
+	    uint32_t( uint8_t( ( (
+			 ( get_chan_64(c,src_data_1) * sx1_w ) +
+			 ( (get_chan_64(c,src_data_m) << 30) ) + // note: src_data_m may be zero
+			 ( sx2_w ? ( get_chan_64(c,src_data_2) * sx2_w ) : uint64_t(0) )
+				  ) * scale + (1lu << 45) ) >> 46 ) ) << (c*8);
 #ifdef RESIZE_DEBUG
 	  uint32_t p1 = src->pels.get()[ rx*src->row_pitch + sx1*src->depth + c ];
 	  uint32_t p2 = src->pels.get()[ rx*src->row_pitch + (sx1+1)*src->depth + c ];
@@ -195,7 +215,7 @@ namespace boda
 #endif
 	  assert( (!sx2_w) || (sx2 < src->w) );
 	}
-	ret->pels.get()[ ry*ret->row_pitch + rx*ret->depth + 3 ] = uint8_t_const_max; // alpha
+	((uint32_t *)ret->pels.get())[ ry*ret->row_pitch_pels + rx ] = dest_val;
       }
     }
     return ret;
