3b86bf1f9666c93b4bce91a27e54318f607f7cb2
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2014-03-25 10:45:50 -0700

    make get_svn_rev use/support git

--- pysrc/get_svn_rev.py
+++ pysrc/get_svn_rev.py
@@ -1,19 +1,32 @@
 # Copyright (c) 2013-2014, Matthew W. Moskewicz <moskewcz@alumni.princeton.edu>; part of Boda framework; see LICENSE
 import os, re, socket;
 
-def get_svn_rev_c_str():
+# unused/untested (used to work, but was refactored during transition to git)
+def svn_get_rev_str():
     info = os.popen( 'cd .. ; svn info' ).readlines()
     svn_rev = None
     for line in info:
         sline = re.split('[ ]+', line)
         if len(sline) == 2 and sline[0] == 'Revision:':
             svn_rev = int(sline[1])
-
     if svn_rev is None:
         raise RuntimeError( "couldn't parse svn info" )
 
+def git_get_rev_str():
+    info = os.popen( 'cd .. ; git rev-parse --verify HEAD --short=6' ).readlines()
+    if len(info) != 1:
+        raise RuntimeError( "couldn't parse git rev info, expected a single line as output, got:" + info )
+    git_rev = info[0].strip()
+    if not git_rev:
+        raise RuntimeError( "couldn't parse git rev info, returned line was only whitespace:" + repr(info) )
+    return git_rev
+
+def get_svn_rev_c_str(): 
+    # FIXME: if we want to support both svn and git, we should autodetect here?
+    rev_str = git_get_rev_str();
     build_host = socket.gethostname()
     ret = ""
-    ret += 'unsigned const svn_rev = %s;\n' % svn_rev 
+    ret += 'char const * const build_rev = "%s";\n' % rev_str 
     ret += 'char const * const build_host = "%s";\n' % build_host
     return ret
+
