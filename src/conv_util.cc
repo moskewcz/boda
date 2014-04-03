96b900d0e36c982a6b4509b8fff8a9cfbdbfa6b6
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2014-04-03 14:38:38 -0700

    conv_ana: add calc of support_padding, padding related changes.
    add options to ingore padding (or not) for both support_sz calc and back/forward sizes calc. remove option to zero padding. note: padding will always print now, even when ignored. this isn't ideal, as we don't store if we've ignored padding or not anywhere.
    TODO: improve printouts from conv_ana to label forward/backward output sections
    TODO: split conv_io_t into support and sz sections?

--- src/conv_util.cc
+++ src/conv_util.cc
@@ -24,11 +24,11 @@ namespace boda
     uint32_t channels; //NESI(default="1",help="number of channels")
     uint32_t groups; //NESI(default="1",help="number of groups (equal partitions of inputs and outputs)")
 
-    u32_pt_t out_sz_to_in_sz( u32_pt_t const & out_sz ) const { 
-      return kern_sz + (out_sz-u32_pt_t(1,1))*stride - in_pad.bnds_sum(); 
+    u32_pt_t out_sz_to_in_sz( u32_pt_t const & out_sz, bool const ignore_padding ) const { 
+      return kern_sz + (out_sz-u32_pt_t(1,1))*stride - (ignore_padding?u32_pt_t():in_pad.bnds_sum());
     }
-    u32_pt_t in_sz_to_out_sz( u32_pt_t const & in_sz ) const { 
-      return (in_sz+in_pad.bnds_sum()-kern_sz)/stride + u32_pt_t(1,1); 
+    u32_pt_t in_sz_to_out_sz( u32_pt_t const & in_sz, bool const ignore_padding ) const { 
+      return (in_sz+(ignore_padding?u32_pt_t():in_pad.bnds_sum())-kern_sz)/stride + u32_pt_t(1,1); 
     }
   };
 
@@ -44,6 +44,7 @@ namespace boda
 
     u32_pt_t support_sz;
     u32_pt_t support_stride;
+    u32_box_t support_pad;
   };
   
   typedef vector< conv_io_t > vect_conv_io_t; 
@@ -53,48 +54,61 @@ namespace boda
   struct conv_pipe_t {
     p_vect_conv_op_t convs;
     vect_conv_io_t conv_ios; // note: imgs.size() == ( convs.size() + 1 )
-    void zero_conv_ios( void ) {
+    conv_pipe_t( p_vect_conv_op_t const & convs_, bool const ignore_padding_for_support ) : convs(convs_) {
       conv_ios.clear();
       conv_ios.resize( convs->size() + 1 );
-      calc_support_sz();
+      calc_support_sz( ignore_padding_for_support );
+    }     
+
+    void zero_conv_ios( void ) {
+      for( vect_conv_io_t::iterator i = conv_ios.begin(); i != conv_ios.end(); ++i ) {
+	i->sz = u32_pt_t(); i->used_sz = u32_pt_t();
+      }
     }
-    void calc_support_sz( void ) {
+
+    void calc_support_sz( bool const ignore_padding ) {
       conv_ios.front().support_sz = u32_pt_t(1,1);
       conv_ios.front().support_stride = u32_pt_t(1,1);
       for( uint32_t i = 0; i != convs->size(); ++i ) {
 	conv_op_t const & cop = convs->at(i);
 	assert_st( cop.kern_sz.both_dims_non_zero() );
-	u32_pt_t const in_sz_1x1 = cop.out_sz_to_in_sz( u32_pt_t(1,1) );
+	u32_pt_t const in_sz_1x1 = cop.out_sz_to_in_sz( u32_pt_t(1,1), ignore_padding );
 	assert_st( in_sz_1x1.both_dims_non_zero() );
 	conv_ios[i+1].support_sz = conv_ios[i].support_sz + ( in_sz_1x1 - u32_pt_t(1,1) )*conv_ios[i].support_stride;
 	conv_ios[i+1].support_stride = conv_ios[i].support_stride*cop.stride;
-      }      
+      }
+      // backward pass to calculate support_pad
+      for( uint32_t i = convs->size(); i; --i ) {
+	conv_op_t const & cop = convs->at(i-1);
+	conv_ios[i-1].support_pad = cop.in_pad + conv_ios[i].support_pad.scale_dims( cop.stride );	
+      }
     }
 
 
-    void calc_sizes_back( u32_pt_t const & out_sz ) {
+    void calc_sizes_back( u32_pt_t const & out_sz, bool const ignore_padding ) {
       zero_conv_ios();
       conv_ios.back().sz = out_sz;
       for( uint32_t i = convs->size(); i; --i ) {
-	conv_ios[i-1].sz = convs->at(i-1).out_sz_to_in_sz( conv_ios[i].sz );
+	conv_ios[i-1].sz = convs->at(i-1).out_sz_to_in_sz( conv_ios[i].sz, ignore_padding );
 	conv_ios[i-1].used_sz = conv_ios[i-1].sz; // by semantics of out_sz_to_in_sz (but checked below)
-	assert_st( conv_ios[i].sz == convs->at(i-1).in_sz_to_out_sz( conv_ios[i-1].sz ) );
+	assert_st( conv_ios[i].sz == convs->at(i-1).in_sz_to_out_sz( conv_ios[i-1].sz, ignore_padding ) );
       }
     }
-    void calc_sizes_forward( u32_pt_t const & in_sz ) {
+    void calc_sizes_forward( u32_pt_t const & in_sz, bool const ignore_padding ) {
       zero_conv_ios(); 
       conv_ios.front().sz = in_sz;
       for( uint32_t i = 0; i != convs->size(); ++i ) {
-	conv_ios[i+1].sz = convs->at(i).in_sz_to_out_sz( conv_ios[i].sz );
-	conv_ios[i].used_sz = convs->at(i).out_sz_to_in_sz( conv_ios[i+1].sz );
+	conv_ios[i+1].sz = convs->at(i).in_sz_to_out_sz( conv_ios[i].sz, ignore_padding );
+	conv_ios[i].used_sz = convs->at(i).out_sz_to_in_sz( conv_ios[i+1].sz, ignore_padding );
       }
     }
     void dump_pipe( std::ostream & out ) {
       out << strprintf( "== BEGIN CONV PIPE ==\n" );
       for( uint32_t i = 0; ; ++i ) {
 	conv_io_t const & cio = conv_ios[i];
-	out << strprintf( "cio: sz=%s support_sz=%s support_stride=%s\n", 
-		str(cio.sz).c_str(), str(cio.support_sz).c_str(), str(cio.support_stride).c_str() );
+	out << strprintf( "cio: sz=%s support_sz=%s support_stride=%s support_pad=%s\n", 
+			  str(cio.sz).c_str(), str(cio.support_sz).c_str(), 
+			  str(cio.support_stride).c_str(), str(cio.support_pad).c_str() );
 	if( conv_ios[i].sz != conv_ios[i].used_sz ) {
 	  out << "  --- DATA DISCARDED --- " << strprintf( "used_sz=%s\n", str(conv_ios[i].used_sz).c_str() );
 	}
@@ -111,18 +125,16 @@ namespace boda
     filename_t out_fn; //NESI(default="%(boda_output_dir)/out.txt",help="text output filename")
     p_vect_conv_op_t convs; //NESI(help="set of convs",req=1)
     // filename_t convs_fn; NESI(help="input: filename for list of convs",req=1)
-    p_uint32_t in_sz; //NESI(help="input size")
-    p_uint32_t out_sz; //NESI(help="output size")
-    uint32_t rem_pad; //NESI(default=0,help="if 1, ignore/reset to 0 any padding specified")
+    p_uint32_t in_sz; //NESI(help="calculate sizes at all layers for the given input size and dump pipe")
+    p_uint32_t out_sz; //NESI(help="calculate sizes at all layers for the given output size and dump pipe")
+    uint32_t ignore_padding_for_support; //NESI(default=0,help="if 1, ignore any padding specified when calculating the support_size for a single pel for each layer")
+    uint32_t ignore_padding_for_sz; //NESI(default=0,help="if 1, ignore any padding specified when calculating the sizes at each layer for the in_sz or out_sz options")
     virtual void main( nesi_init_arg_t * nia ) { 
       p_ofstream out = ofs_open( out_fn.exp );
       //(*out) << convs << "\n";
-      if( rem_pad ) {
-	for( vect_conv_op_t::iterator i = convs->begin(); i != convs->end(); ++i ) { (*i).in_pad = u32_box_t(); }
-      }
-      conv_pipe_t cp{convs};
-      if( out_sz ) { cp.calc_sizes_back( u32_pt_t( *out_sz, *out_sz ) ); cp.dump_pipe(*out); }
-      if( in_sz ) { cp.calc_sizes_forward( u32_pt_t( *in_sz, *in_sz ) ); cp.dump_pipe(*out); }
+      conv_pipe_t cp{convs,ignore_padding_for_support};
+      if( out_sz ) { cp.calc_sizes_back( u32_pt_t( *out_sz, *out_sz ), ignore_padding_for_sz ); cp.dump_pipe(*out); }
+      if( in_sz ) { cp.calc_sizes_forward( u32_pt_t( *in_sz, *in_sz ), ignore_padding_for_sz ); cp.dump_pipe(*out); }
     }
   };
 
