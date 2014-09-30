44405f48bb6a20dd0d754e85663afd71a998ba91
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2014-09-30 16:48:41 -0700

    boda_completion: not working yet, but let's remove the debug printout for now anyway

--- boda_completion.bash
+++ boda_completion.bash
@@ -3,7 +3,7 @@ _boda()
   _boda_commands=$(boda compsup)
   local cur prev
   COMPREPLY=()
-  echo "COMP_WORDS ${COMP_WORDS[1]}"
+  #echo "COMP_WORDS ${COMP_WORDS[1]}"
   cur="${COMP_WORDS[COMP_CWORD]}"
   COMPREPLY=( $(compgen -W "${_boda_commands}" -- ${cur}) )
   return 0
