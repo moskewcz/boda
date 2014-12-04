b9fb617dd544df8c74e45b46f79102f857f57603
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2014-12-03 20:31:36 -0800

    test_dense: more progress; in intermediate state

--- src/test_dense.cc
+++ src/test_dense.cc
@@ -8,8 +8,27 @@
 #include"caffeif.H"
 #include"conv_util.H"
 #include"rand_util.H"
+#include"timers.H"
 
 namespace boda {
+
+  uint32_t const inmc = 123U+(117U<<8)+(104U<<16)+(255U<<24); // RGBA
+
+  template< typename T >
+  shared_ptr< nda_T< T > > copy_clip( shared_ptr< nda_T< T > > in, dims_t const & b, dims_t const & e ) {
+    shared_ptr< nda_T< T > > ret( new nda_T< T > );
+    dims_t ret_dims = e - b;
+    ret->set_dims( ret_dims );
+    dims_iter_t rdi( ret_dims );
+    //printf( "dims=%s in->dims=%s\n", str(dims).c_str(), str(in->dims).c_str() );
+    for( dims_iter_t di( b, e ) ; ; ) { 
+      ret->at(rdi.di) = in->at(di.di); 
+      if( !di.next() ) { break; } 
+      assert_st( rdi.next() );
+    }
+    return ret;
+  }
+    
 
   struct test_dense_t : virtual public nesi, public has_main_t // NESI( help="test dense vs. sparse CNN eval",
 			// bases=["has_main_t"], type_id="test_dense")
@@ -31,7 +50,16 @@ namespace boda {
     p_conv_pipe_t conv_pipe;
     p_vect_conv_io_t conv_ios;
 
-    
+    p_conv_pipe_t conv_pipe_dense;
+    p_vect_conv_io_t conv_ios_dense;
+
+    void get_pipe_and_ios( p_run_cnet_t const & run_cnet, p_conv_pipe_t & conv_pipe, p_vect_conv_io_t & conv_ios ) {
+      conv_pipe = run_cnet->get_pipe();
+      //conv_pipe->dump_pipe( std::cout );
+      conv_ios = conv_pipe->calc_sizes_forward( run_cnet->in_sz, 0 ); 
+      conv_pipe->dump_ios( std::cout, conv_ios );
+    }
+
     virtual void main( nesi_init_arg_t * nia ) {
       imgs->load_all_imgs();
       run_cnet->setup_cnet(); 
@@ -43,37 +71,79 @@ namespace boda {
       in_img = make_p_img_t( run_cnet->in_sz );
       in_img_dense = make_p_img_t( run_cnet_dense->in_sz );
 
-      conv_pipe = run_cnet->get_pipe();
-      //conv_pipe->dump_pipe( std::cout );
-      conv_ios = conv_pipe->calc_sizes_forward( run_cnet->in_sz, 0 ); 
-      //conv_pipe->dump_ios( std::cout, conv_ios );
+      get_pipe_and_ios( run_cnet, conv_pipe, conv_ios );
+      get_pipe_and_ios( run_cnet_dense, conv_pipe_dense, conv_ios_dense );
       
       boost::random::mt19937 gen;
 
       uint32_t tot_wins = 0;
       for( vect_p_img_t::const_iterator i = imgs->all_imgs->begin(); i != imgs->all_imgs->end(); ++i ) {
 	if( !(*i)->sz.both_dims_ge( run_cnet->in_sz ) ) { continue; } // img too small to sample. assert? warn?
-	u32_pt_t const samp_sz = (*i)->sz - run_cnet->in_sz;
-	u32_pt_t const samp_nc = random_pt( samp_sz, gen );
-	++tot_wins;
-	comp_win( (*i), samp_nc );
+	printf( "(*i)->sz=%s\n", str((*i)->sz).c_str() );
+	// run net on entire input image
+	in_img_dense->fill_with_pel( inmc );
+	img_copy_to_clip( (*i).get(), in_img_dense.get() );
+	subtract_mean_and_copy_img_to_batch( run_cnet_dense->in_batch, 0, in_img_dense );
+	p_nda_float_t out_batch_dense;
+	{
+	  timer_t t1("dense_cnn");
+	  out_batch_dense = run_cnet_dense->run_one_blob_in_one_blob_out();
+	}
+      
+	for( uint32_t wix = 0; wix != wins_per_image; ++wix ) {
+	  u32_pt_t const samp_nc_max = (*i)->sz - run_cnet->in_sz;
+	  u32_pt_t const samp_nc = random_pt( samp_nc_max, gen );
+	  ++tot_wins;
+	  comp_win( out_batch_dense, (*i), samp_nc );
+	}
       }
     }
-    void comp_win( p_img_t const & img, u32_pt_t const & nc ) {
-      printf( "nc=%s\n", str(nc).c_str() );      
+    void comp_win( p_nda_float_t out_batch_dense, p_img_t const & img, u32_pt_t const & nc ) {
+      dims_t const & obd_dense = out_batch_dense->dims;
+      assert( obd_dense.sz() == 4 );
+      assert( obd_dense.dims(0) == 1 ); // one image
+      //printf( "nc=%s\n", str(nc).c_str() );   
+      u32_box_t in_box{ nc, nc + run_cnet->in_sz };
+      conv_support_info_t const & ol_csi_dense = conv_pipe_dense->conv_sis.back();
+      i32_box_t feat_box_dense;
+      in_box_to_out_box( feat_box_dense, in_box, cm_valid, ol_csi_dense );
+
+      u32_pt_t const & feat_sz = conv_ios->back().sz;
+      if( i32_to_u32(feat_box_dense.sz()) != feat_sz ) { return; }
+      
+      printf( "feat_box_dense=%s\n", str(feat_box_dense).c_str() );
       // run net on just sample area
       img_copy_to_clip( img.get(), in_img.get(), {}, nc );
       subtract_mean_and_copy_img_to_batch( run_cnet->in_batch, 0, in_img );
-      p_nda_float_t out_batch = run_cnet->run_one_blob_in_one_blob_out();
+      p_nda_float_t out_batch;
+      {
+	timer_t t1("sparse_cnn");
+	out_batch = run_cnet->run_one_blob_in_one_blob_out();
+	dims_t const & obd = out_batch->dims;
+	assert( obd.sz() == 4 );
+	assert( obd.dims(0) == 1 ); // one image
+	assert( obd.dims(1) == obd_dense.dims(1) ); // same chans
 
-      uint32_t const inmc = 123U+(117U<<8)+(104U<<16)+(255U<<24); // RGBA
-
-      // run net on entire input image
-      in_img_dense->fill_with_pel( inmc );
-      img_copy_to_clip( img.get(), in_img_dense.get() );
-      subtract_mean_and_copy_img_to_batch( run_cnet_dense->in_batch, 0, in_img_dense );
-      p_nda_float_t out_batch_dense = run_cnet_dense->run_one_blob_in_one_blob_out();
-
+	dims_t b(4);
+	dims_t e = obd_dense;
+	b.dims(2) = feat_box_dense.p[0].d[1];
+	b.dims(3) = feat_box_dense.p[0].d[0];
+	e.dims(2) = b.dims(2) + feat_sz.d[1];
+	e.dims(3) = b.dims(3) + feat_sz.d[0];
+	p_nda_float_t from_dense = copy_clip( out_batch_dense, b, e );
+	printf( "ssds_str(from_dense,out_batch)=%s\n", str(ssds_str(from_dense,out_batch)).c_str() );
+#if 0	
+	for( uint32_t c = 0; c < obd.dims(1); ++c ) {
+	  for( uint32_t y = 0; y < feat_sz.d[1]; ++y ) {
+	    for( uint32_t x = 0; x < feat_sz.d[0]; ++x ) {
+	      float const v = out_batch->at4(0,c,y,x);
+	      float const vd = out_batch_dense->at4(0,c,y+feat_box_dense.p[0].d[1],x+feat_box_dense.p[0].d[0]);
+	      printf( "v=%s vd=%s\n", str(v).c_str(), str(vd).c_str() );
+	    }
+	  }
+	}
+#endif
+      }
     }
   };
 
