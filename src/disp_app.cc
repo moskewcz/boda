a9082ff43aa97a8c780a137b1dd98a5ffd075186
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-02-11 17:54:52 -0800

    display_pil: questionable code for skipping display of images in various semi-hardcoded cases.

--- src/disp_app.cc
+++ src/disp_app.cc
@@ -39,6 +39,9 @@ namespace boda
     while( !(val < max_val) ) { val -= max_val; }
   }
 
+  // scoring experiments example command lines:
+  // ../../lib/boda display_pil --pil-fn="%(pascal_data_dir)/ImageSets/Main/%%s_test.txt" --pascal-classes-fn="%(boda_test_dir)/pascal_classes.txt" --rand-winds=0 --fps=1000 --prc-png-fn="" --do-score=1
+
   struct display_pil_t : virtual public nesi, public load_imgs_from_pascal_classes_t, public has_main_t // NESI(
 			 // help="display PASCAL VOC list of images in video window",
 			 // bases=["load_imgs_from_pascal_classes_t","has_main_t"], type_id="display_pil")
@@ -47,7 +50,7 @@ namespace boda
     double fps; //NESI(default=5,help="frames to (try to ) send to display per second (note: independant of display rate)")
     uint32_t rand_winds; //NESI(default=0,help="if set, display 1/2 image size random windows instead of full image")
     uint32_t auto_adv; //NESI(default=1,help="if set, slideshow mode")
-    uint32_t do_score; //NESI(default=1,help="if set, run scoring")
+    uint32_t do_score; //NESI(default=1,help="if set, run scoring. if == 2, quit after scoring.")
     disp_win_t disp_win;
     p_vect_p_img_t disp_imgs;
     p_deadline_timer_t frame_timer;
@@ -65,7 +68,9 @@ namespace boda
       for( vect_string::const_iterator i = (*classes).begin(); i != (*classes).end(); ++i ) {
 	p_vect_scored_det_t const & sds = scored_dets->at( i - classes->begin() );
 	vect_base_scored_det_t & img_sds = sds->get_per_img_sds( img_info->ix );
-	img_sds.push_back( base_scored_det_t{u32_box_t{{50,50},{200,200}},10.4} );
+	p_img_t const & img = img_info->img;
+	
+	img_sds.push_back( base_scored_det_t{u32_box_t{img->sz.scale_and_round(0.14),img->sz.scale_and_round(1.0 - 0.14)},10.4} );
 	//oct_dfc( cout, dpm_fast_cascade_dir.exp, scored_dets->back(), img_info->full_fn, img_info->ix );
       }
     }
@@ -75,47 +80,53 @@ namespace boda
       assert( !ec );
       frame_timer->expires_at( frame_timer->expires_at() + frame_dur );
       frame_timer->async_wait( bind( &display_pil_t::on_frame, this, _1 ) ); 
-      assert( cur_img_ix < all_imgs->size() );
-      p_img_t const & img = all_imgs->at(cur_img_ix);
-      u32_pt_t src_pt, dest_pt;
-      u32_pt_t copy_sz{u32_pt_t_const_max};
-      if( rand_winds ) {
-	copy_sz = ceil_div( img->sz, {2,2} );
-	src_pt = random_pt( img->sz - copy_sz, gen );
-	dest_pt = random_pt( disp_imgs->at(0)->sz - copy_sz, gen );
-      }
-      img_copy_to_clip( img.get(), disp_imgs->at(0).get(), dest_pt, src_pt, copy_sz );
-      assert_st( cur_img_ix < img_db->img_infos.size() );
-      p_img_info_t img_info = img_db->img_infos[cur_img_ix];
-      p_vect_anno_t annos( new vect_anno_t );
-      for( vect_string::const_iterator i = (*classes).begin(); i != (*classes).end(); ++i ) {
-	string const & cn = *i;
-	vect_gt_det_t const & gt_dets = img_info->gt_dets[cn];	
-	vect_gt_match_t * gtms = 0;
-	// annotate SDs
-	if( do_score ) {
-	  p_vect_scored_det_t const & sds = scored_dets->at( i - classes->begin() );
-	  gtms = &sds->get_gtms( img_info->ix, gt_dets.size() );
+      while( 1 ) {
+	bool skip_img = 0;
+	assert( cur_img_ix < all_imgs->size() );
+	p_img_t const & img = all_imgs->at(cur_img_ix);
+	u32_pt_t src_pt, dest_pt;
+	u32_pt_t copy_sz{u32_pt_t_const_max};
+	if( rand_winds ) {
+	  copy_sz = ceil_div( img->sz, {2,2} );
+	  src_pt = random_pt( img->sz - copy_sz, gen );
+	  dest_pt = random_pt( disp_imgs->at(0)->sz - copy_sz, gen );
+	}
+	img_copy_to_clip( img.get(), disp_imgs->at(0).get(), dest_pt, src_pt, copy_sz );
+	assert_st( cur_img_ix < img_db->img_infos.size() );
+	p_img_info_t img_info = img_db->img_infos[cur_img_ix];
+	p_vect_anno_t annos( new vect_anno_t );
+	for( vect_string::const_iterator i = (*classes).begin(); i != (*classes).end(); ++i ) {
+	  string const & cn = *i;
+	  vect_gt_det_t const & gt_dets = img_info->gt_dets[cn];	
+	  vect_gt_match_t * gtms = 0;
+	  // annotate SDs
+	  if( do_score ) {
+	    p_vect_scored_det_t const & sds = scored_dets->at( i - classes->begin() );
+	    gtms = &sds->get_gtms( img_info->ix, gt_dets.size() );
 	  
-	  vect_base_scored_det_t & img_sds = sds->get_per_img_sds( img_info->ix );
-	  for( vect_base_scored_det_t::const_iterator i = img_sds.begin(); i != img_sds.end(); ++i ) {
-	    annos->push_back( anno_t{u32_to_i32(*i), rgba_to_pel(40,40,170), 0, cn + "=" + str(i->score), rgba_to_pel(220,220,255) } );
-	  }	
-	}
+	    vect_base_scored_det_t & img_sds = sds->get_per_img_sds( img_info->ix );
+	    for( vect_base_scored_det_t::const_iterator i = img_sds.begin(); i != img_sds.end(); ++i ) {
+	      annos->push_back( anno_t{u32_to_i32(*i), rgba_to_pel(40,40,170), 0, cn + "=" + str(i->score), rgba_to_pel(220,220,255) } );
+	    }	
+	  }
 
-	// annotate GTs
-	for( uint32_t i = 0; i != gt_dets.size(); ++i ) {
-	  bool const is_matched = (!gtms) || gtms->at(i).matched;
-	  uint32_t gt_color = is_matched ? rgba_to_pel(40,170,40) : rgba_to_pel(170,40,40);
-	  annos->push_back( anno_t{u32_to_i32(gt_dets[i]), gt_color, 0, cn, rgba_to_pel(220,220,255) } );
-	  if( gtms && is_matched ) { auto_adv = 0; }
+	  // annotate GTs
+	  skip_img = gt_dets.empty(); // skip if no gts
+	  for( uint32_t i = 0; i != gt_dets.size(); ++i ) {
+	    bool const is_matched = (!gtms) || gtms->at(i).matched;
+	    uint32_t gt_color = is_matched ? rgba_to_pel(40,170,40) : rgba_to_pel(170,40,40);
+	    annos->push_back( anno_t{u32_to_i32(gt_dets[i]), gt_color, 0, cn, rgba_to_pel(220,220,255) } );
+	    if( gtms && is_matched ) { skip_img = 1; }
+	  }
 	}
-
+	if( skip_img ) { mod_adj( cur_img_ix, all_imgs->size(), 1 ); }
+	else {
+	  disp_win.update_img_annos( 0, annos );
+	  if( auto_adv ) { mod_adj( cur_img_ix, all_imgs->size(), 1 ); }
+	  disp_win.update_disp_imgs();
+	  break;
+	}
       }
-
-      disp_win.update_img_annos( 0, annos );
-      if( auto_adv ) { mod_adj( cur_img_ix, all_imgs->size(), 1 ); }
-      disp_win.update_disp_imgs();
     }
     void on_quit( error_code const & ec ) { get_io( &disp_win ).stop(); }
 
@@ -155,6 +166,7 @@ namespace boda
 	for( vect_p_vect_scored_det_t::iterator i = scored_dets->begin(); i != scored_dets->end(); ++i ) { 
 	  (*i)->merge_per_img_sds(); }
 	img_db->score_results( scored_dets, prc_txt_fn.exp, prc_png_fn.exp );
+	if( do_score == 2 ) { return; }
       }
 
       disp_imgs = disp_win.disp_setup( {{640,480}} );
