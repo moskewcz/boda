31d38bbac4597c4d033137b26e0f7fac3713c26d
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-02-11 16:59:29 -0800

    display_pil: experimenting with scoring/display of detection results

--- src/disp_app.cc
+++ src/disp_app.cc
@@ -47,14 +47,29 @@ namespace boda
     double fps; //NESI(default=5,help="frames to (try to ) send to display per second (note: independant of display rate)")
     uint32_t rand_winds; //NESI(default=0,help="if set, display 1/2 image size random windows instead of full image")
     uint32_t auto_adv; //NESI(default=1,help="if set, slideshow mode")
+    uint32_t do_score; //NESI(default=1,help="if set, run scoring")
     disp_win_t disp_win;
     p_vect_p_img_t disp_imgs;
     p_deadline_timer_t frame_timer;
     time_duration frame_dur;
 
+    display_pil_t( void ) { }
     uint32_t cur_img_ix;
     boost::random::mt19937 gen;
-    display_pil_t( void ) { }
+
+    filename_t prc_txt_fn; //NESI(default="%(boda_output_dir)/prc_",help="output: text prc curve base filename")
+    filename_t prc_png_fn; //NESI(default="%(boda_output_dir)/mAP_",help="output: png prc curve base filename")
+    p_vect_p_vect_scored_det_t scored_dets;
+
+    void score_img( p_img_info_t const & img_info ) {
+      for( vect_string::const_iterator i = (*classes).begin(); i != (*classes).end(); ++i ) {
+	p_vect_scored_det_t const & sds = scored_dets->at( i - classes->begin() );
+	vect_base_scored_det_t & img_sds = sds->get_per_img_sds( img_info->ix );
+	img_sds.push_back( base_scored_det_t{u32_box_t{{50,50},{200,200}},10.4} );
+	//oct_dfc( cout, dpm_fast_cascade_dir.exp, scored_dets->back(), img_info->full_fn, img_info->ix );
+      }
+    }
+
     void on_frame( error_code const & ec ) {
       if( ec ) { return; }
       assert( !ec );
@@ -70,17 +85,34 @@ namespace boda
 	dest_pt = random_pt( disp_imgs->at(0)->sz - copy_sz, gen );
       }
       img_copy_to_clip( img.get(), disp_imgs->at(0).get(), dest_pt, src_pt, copy_sz );
-      // annotate GTs
       assert_st( cur_img_ix < img_db->img_infos.size() );
       p_img_info_t img_info = img_db->img_infos[cur_img_ix];
       p_vect_anno_t annos( new vect_anno_t );
-      for( name_vect_gt_det_map_t::const_iterator i = img_info->gt_dets.begin(); i != img_info->gt_dets.end(); ++i ) {
-	vect_gt_det_t const & gt_dets = i->second; // note: may be created here (i.e. may be empty)
-	string const & cn = i->first;
-	for( vect_gt_det_t::const_iterator i = gt_dets.begin(); i != gt_dets.end(); ++i ) {
-	  annos->push_back( anno_t{u32_to_i32(*i), rgba_to_pel(170,40,40), 0, cn, rgba_to_pel(220,220,255) } );
+      for( vect_string::const_iterator i = (*classes).begin(); i != (*classes).end(); ++i ) {
+	string const & cn = *i;
+	vect_gt_det_t const & gt_dets = img_info->gt_dets[cn];	
+	vect_gt_match_t * gtms = 0;
+	// annotate SDs
+	if( do_score ) {
+	  p_vect_scored_det_t const & sds = scored_dets->at( i - classes->begin() );
+	  gtms = &sds->get_gtms( img_info->ix, gt_dets.size() );
+	  
+	  vect_base_scored_det_t & img_sds = sds->get_per_img_sds( img_info->ix );
+	  for( vect_base_scored_det_t::const_iterator i = img_sds.begin(); i != img_sds.end(); ++i ) {
+	    annos->push_back( anno_t{u32_to_i32(*i), rgba_to_pel(40,40,170), 0, cn + "=" + str(i->score), rgba_to_pel(220,220,255) } );
+	  }	
 	}
+
+	// annotate GTs
+	for( uint32_t i = 0; i != gt_dets.size(); ++i ) {
+	  bool const is_matched = (!gtms) || gtms->at(i).matched;
+	  uint32_t gt_color = is_matched ? rgba_to_pel(40,170,40) : rgba_to_pel(170,40,40);
+	  annos->push_back( anno_t{u32_to_i32(gt_dets[i]), gt_color, 0, cn, rgba_to_pel(220,220,255) } );
+	  if( gtms && is_matched ) { auto_adv = 0; }
+	}
+
       }
+
       disp_win.update_img_annos( 0, annos );
       if( auto_adv ) { mod_adj( cur_img_ix, all_imgs->size(), 1 ); }
       disp_win.update_disp_imgs();
@@ -110,6 +142,21 @@ namespace boda
     virtual void main( nesi_init_arg_t * nia ) {
       load_all_imgs();
 
+      if( do_score ) {
+	// setup scored_dets
+	scored_dets.reset( new vect_p_vect_scored_det_t );
+	for( vect_string::const_iterator i = (*classes).begin(); i != (*classes).end(); ++i ) {
+	  scored_dets->push_back( p_vect_scored_det_t( new vect_scored_det_t( *i ) ) );
+	}
+	for (uint32_t ix = 0; ix < img_db->img_infos.size(); ++ix) {
+	  p_img_info_t img_info = img_db->img_infos[ix];
+	  score_img( img_info );
+	}	
+	for( vect_p_vect_scored_det_t::iterator i = scored_dets->begin(); i != scored_dets->end(); ++i ) { 
+	  (*i)->merge_per_img_sds(); }
+	img_db->score_results( scored_dets, prc_txt_fn.exp, prc_png_fn.exp );
+      }
+
       disp_imgs = disp_win.disp_setup( {{640,480}} );
       
       cur_img_ix = 0;
@@ -122,6 +169,8 @@ namespace boda
       register_lb_handler( disp_win, &display_pil_t::on_lb, this );
 
       io.run();
+
+
     }
 
   };
