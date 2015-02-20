f8bbaee89473f55822ce5e17889e2b17d0e62261
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-02-19 16:31:22 -0800

    makefile: more work on obj_list options

--- pysrc/boda_make_util.py
+++ pysrc/boda_make_util.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2015, Matthew W. Moskewicz <moskewcz@alumni.princeton.edu>; part of Boda framework; see LICENSE
-import os
+import os, re
 
 # one complication/flaw of the gcc generated .d files + the make
 # system we are using is that when a source file is changed in a way
@@ -43,18 +43,67 @@ class DepFileProc( object ):
 ospj = os.path.join
 
 class GenObjList( object ):
+
+    def next_line( self ):
+        while 1:
+            assert self.next_ol_line <= len(self.ol_lines)
+            if self.next_ol_line == len(self.ol_lines):
+                self.cur_line = None
+                return False
+            line = self.ol_lines[self.next_ol_line]
+            self.next_ol_line += 1
+            comment_char_idx = line.find('#')
+            if comment_char_idx != -1: line = line[:comment_char_idx] # remove comment if any
+            line = line.strip()
+            if not line: continue # skip blank lines
+            self.cur_line = line
+            return True
+
+    def parse_error( self, msg ):
+        assert self.cur_line is not None
+        raise ValueError( "obj_list parse error on line %s: %s\nline was:%s" %( self.next_ol_line, msg, self.cur_line) );
+
+    def get_section_start( self ):
+        if self.cur_line is None: return None
+        sec_re_match = re.match( '\[\s*(.+)\s*\]$', self.cur_line )
+        if not sec_re_match: return None
+        return sec_re_match.group(1)
+
+    def at_section_start_or_eof( self ):
+        if self.cur_line is None: return True
+        return not ( self.get_section_start() is None )
+
+    def parse_objs( self ):
+        while not self.at_section_start_or_eof():
+            self.gen_objs.append( self.cur_line )
+            self.next_line();
+    
+    def parse_dep( self, dep_name ):
+        if dep_name in self.deps: self.parse_error( "duplicate dep section" )
+        dep_lines = []
+        while not self.at_section_start_or_eof():
+            dep_lines.append( self.cur_line )
+            self.next_line();
+        self.deps[dep_name] = dep_lines
+
     def __init__( self ):    
+        self.deps = {}
         self.gen_objs = []
         self.src_dir = ospj('..','src')
         ol_fn = ospj(self.src_dir,"obj_list")
-        ol_lines = open( ol_fn ).readlines()
-        if not ol_lines: raise ValueError( "empty obj_list file at: " + ol_fn )
-        for ol_line in ol_lines:
-            comment_char_idx = ol_line.find('#')
-            if comment_char_idx != -1: ol_line = ol_line[:comment_char_idx] # remove comment if any
-            ol_parts = ol_line.split()
-            if not ol_parts: continue # skip blank lines
-            self.gen_objs.append( ol_parts[0] )
+        self.ol_lines = open( ol_fn ).readlines()
+        if not self.ol_lines: raise ValueError( "empty obj_list file at: " + ol_fn )
+        self.next_ol_line = 0
+        self.next_line()
+
+        while self.cur_line is not None:
+            sec_name = self.get_section_start()
+            if sec_name is None: self.parse_error( "expecting start of section" )
+            self.next_line() # consume section start line
+            if sec_name == "objs": self.parse_objs()
+            else: self.parse_dep( sec_name )
+        
+        if not self.gen_objs: raise ValueError( "obj_list error: [objs] section missing or empty" )
             
         # add in any generated c++ file that need top-level compilation. FIXME: probably this shouldn't be hard-coded here.
         self.gen_objs.append( 'build_info.o' ) 
