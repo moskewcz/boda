1077dafbcd40cfacc409e27687c55cb6a5db101d
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-02-24 21:04:09 -0800

    scan_lmdb: simplify / remove unused code

--- pysrc/scan_lmdb.py
+++ pysrc/scan_lmdb.py
@@ -1,37 +1,8 @@
-
-# Roughly approximates some of Symas microbenchmark.
-
-import multiprocessing
-import os
-import random
-import shutil
+#!/usr/bin/env python
 import sys
-import tempfile
-import time
-
-try:
-    import affinity
-except:
-    affinity = False
 import lmdb
 
-
-USE_SPARSE_FILES = sys.platform != 'darwin'
-DB_PATH = sys.argv[1]
-MAX_KEYS = int(4e6)
-
-def open_env():
-    return lmdb.open(DB_PATH,
-        map_size=1048576 * 1024,
-        metasync=False,
-        sync=False,
-        map_async=True,
-        writemap=USE_SPARSE_FILES)
-
-
-env = open_env()
-#with env.begin() as txn:
-#    keys = list(txn.cursor().iternext(values=False))
+env =  lmdb.open( sys.argv[1], map_size=1048576 * 1024, metasync=False, sync=False, map_async=True, writemap=True )
 
 with env.begin() as txn:
     cursor = txn.cursor()
