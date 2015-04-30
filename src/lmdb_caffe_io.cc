2d8945ad6cf1995a3140a5a80f5dfe922dcb41e6
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-04-30 15:34:07 -0700

    lmdb_caff_io: add display_lmdb mode. may move/remove later.

--- src/lmdb_caffe_io.cc
+++ src/lmdb_caffe_io.cc
@@ -3,6 +3,8 @@
 #include"lmdbif.H"
 #include"caffepb.H"
 #include"has_main.H"
+#include"disp_util.H"
+#include"asio_util.H"
 
 namespace boda 
 {
@@ -15,21 +17,95 @@ namespace boda
     uint64_t num_read; //NESI(default=2,help="read this many records")
 
     lmdb_state_t lmdb;
-    void main( nesi_init_arg_t * nia ) { 
+
+    p_datum_t read_next_datum( void ) {
+      MDB_val key, data;      
+      bool const ret = lmdb.cursor_next( &key, &data );
+      if( !ret ) { return p_datum_t(); }
+      p_datum_t datum = parse_datum( data.mv_data, data.mv_size );
+      return datum;
+    }
+
+    void lmdb_open_and_start_read_pass( void ) {
       lmdb.env_open( db_fn.exp, MDB_RDONLY ); 
-      MDB_val key, data;
       lmdb.txn_begin( MDB_RDONLY );
       lmdb.cursor_open();
-      uint64_t num = 0;
-      while( lmdb.cursor_next( &key, &data ) ) {
-	//printf( "key.mv_size=%s data.mv_size=%s\n", str(key.mv_size).c_str(), str(data.mv_size).c_str() );
-	p_datum_t datum = parse_datum( data.mv_data, data.mv_size );
-	//printf( "datum->label=%s\n", str(datum->label).c_str() );
-	++num; if( num == num_read ) { break; }
-      }
-      lmdb.cursor_close();
-      lmdb.txn_abort();
     }
+    
+    void main( nesi_init_arg_t * nia ) { 
+      lmdb_open_and_start_read_pass();
+      for( uint64_t i = 0; i != num_read; ++i ) {
+	if( !read_next_datum() ) { break; } // quit if we run out of data early
+      }
+    }
+
+  };
+
+  struct display_lmdb_t : virtual public nesi, public lmdb_parse_datums_t // NESI(
+			 // help="display caffe lmdb of images in video window",
+			 // bases=["lmdb_parse_datums_t"], type_id="display_lmdb")
+  {
+    virtual cinfo_t const * get_cinfo( void ) const; // required declaration for NESI support
+    double fps; //NESI(default=5,help="frames to (try to ) send to display per second (note: independant of display rate)")
+    uint32_t rand_winds; //NESI(default=0,help="if set, display 1/2 image size random windows instead of full image")
+    uint32_t auto_adv; //NESI(default=1,help="if set, slideshow mode")
+    disp_win_t disp_win;
+    p_vect_p_img_t disp_imgs;
+    p_deadline_timer_t frame_timer;
+    time_duration frame_dur;
+
+    display_lmdb_t( void ) { }
+
+    void on_frame( error_code const & ec ) {
+      if( ec ) { return; }
+      assert( !ec );
+      frame_timer->expires_at( frame_timer->expires_at() + frame_dur );
+      frame_timer->async_wait( bind( &display_lmdb_t::on_frame, this, _1 ) ); 
+
+      p_datum_t next_datum = read_next_datum();
+      if( !next_datum ) { return; }
+      p_img_t img = datum_to_img( next_datum );
+
+      if( !rand_winds ) { disp_imgs->at(0)->fill_with_pel( grey_to_pel( 128 ) ); }
+      img_copy_to_clip( img.get(), disp_imgs->at(0).get() );
+      disp_win.update_disp_imgs();
+    }
+    void on_quit( error_code const & ec ) { get_io( &disp_win ).stop(); }
+
+    void on_lb( error_code const & ec ) { 
+      register_lb_handler( disp_win, &display_lmdb_t::on_lb, this ); // re-register handler for next event
+      lb_event_t const & lbe = get_lb_event(&disp_win);
+      //printf( "lbe.is_key=%s lbe.keycode=%s\n", str(lbe.is_key).c_str(), str(lbe.keycode).c_str() );
+      bool unknown_command = 0;
+      if( 0 ) { }
+      //else if( lbe.is_key && (lbe.keycode == 'd') ) { mod_adj( cur_img_ix, img_db->img_infos.size(),  1 ); auto_adv=0; }
+      //else if( lbe.is_key && (lbe.keycode == 'a') ) { mod_adj( cur_img_ix, img_db->img_infos.size(), -1 ); auto_adv=0; }
+      else if( lbe.is_key && (lbe.keycode == 'p') ) { auto_adv ^= 1; }
+      else if( lbe.is_key ) { // unknown command handlers
+	unknown_command = 1; 
+	printf("unknown/unhandled UI key event with keycode = %s\n", str(lbe.keycode).c_str() ); } 
+      else { unknown_command = 1; printf("unknown/unhandled UI event\n"); } // unknown command
+      if( !unknown_command ) { // if known command, force redisplay now
+	frame_timer->cancel();
+	frame_timer->expires_from_now( time_duration() );
+	frame_timer->async_wait( bind( &display_lmdb_t::on_frame, this, _1 ) ); 
+      }
+    }
+
+    virtual void main( nesi_init_arg_t * nia ) {
+      lmdb_open_and_start_read_pass();
+      disp_imgs = disp_win.disp_setup( {{300,300}} );      
+      //cur_img_ix = 0;
+      io_service_t & io = get_io( &disp_win );
+      frame_timer.reset( new deadline_timer_t( io ) );
+      frame_dur = microseconds( 1000 * 1000 / fps );
+      frame_timer->expires_from_now( time_duration() );
+      frame_timer->async_wait( bind( &display_lmdb_t::on_frame, this, _1 ) );
+      register_quit_handler( disp_win, &display_lmdb_t::on_quit, this );
+      register_lb_handler( disp_win, &display_lmdb_t::on_lb, this );
+      io.run();
+    }
+
   };
 
 #include"gen/lmdb_caffe_io.cc.nesi_gen.cc"
