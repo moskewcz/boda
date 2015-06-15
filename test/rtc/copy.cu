2f447ecf37b98939a9df36eca13597e2b703c683
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-06-15 14:43:57 -0700

    rtc: more uint32_t -> int32_t changes for lrn and copy

--- test/rtc/copy.cu
+++ test/rtc/copy.cu
@@ -1,8 +1,8 @@
 // each thread: computes outputs across chan dim, using inputs across chan dim
 extern "C"  __global__ void %(cu_func_name)( float const * const in, float * const out ) {
-  uint32_t const in_ix = blockDim.x * blockIdx.x + threadIdx.x;
+  int32_t const in_ix = blockDim.x * blockIdx.x + threadIdx.x;
   if( in_ix >= %(in_ix_sz) ) { return; }
-  uint32_t const out_ix = %(in_ix_img)*%(out_ix_img_sz) + (%(in_ix_chan)+%(ocix))*%(out_ix_chan_sz) +
+  int32_t const out_ix = %(in_ix_img)*%(out_ix_img_sz) + (%(in_ix_chan)+%(ocix))*%(out_ix_chan_sz) +
     %(in_ix_y)*%(out_ix_y_sz) + %(in_ix_x)*%(out_ix_x_sz);  
   out[out_ix] = in[in_ix];
 }
--- test/rtc/lrn.cu
+++ test/rtc/lrn.cu
@@ -1,20 +1,20 @@
 // each thread: computes outputs across chan dim, using inputs across chan dim
 extern "C"  __global__ void %(cu_func_name)( float const * const in, float * const out ) {
-  uint32_t const tix = blockDim.x * blockIdx.x + threadIdx.x;
+  int32_t const tix = blockDim.x * blockIdx.x + threadIdx.x;
   if( tix >= %(tix_sz) ) { return; }
   // iteratate over chans
   float ls_buf[%(local_size)] = {0.0f};
-  uint32_t const hls = %(local_size) >> 1;
-  uint32_t const out_base_ix = %(tix_img)*%(out_ix_img_sz) + %(tix_y)*%(out_ix_y_sz) + %(tix_x)*%(out_ix_x_sz);  
-  for( uint32_t in_chan_ix = 0; in_chan_ix < %(out_ix_chan_dim) + hls; ++in_chan_ix ) {
-    uint32_t const in_off = in_chan_ix*%(out_ix_chan_sz);
-    uint32_t const lsb_ix = in_chan_ix %% %(local_size);
+  int32_t const hls = %(local_size) >> 1;
+  int32_t const out_base_ix = %(tix_img)*%(out_ix_img_sz) + %(tix_y)*%(out_ix_y_sz) + %(tix_x)*%(out_ix_x_sz);  
+  for( int32_t in_chan_ix = 0; in_chan_ix < %(out_ix_chan_dim) + hls; ++in_chan_ix ) {
+    int32_t const in_off = in_chan_ix*%(out_ix_chan_sz);
+    int32_t const lsb_ix = in_chan_ix %% %(local_size);
     ls_buf[lsb_ix] = (in_chan_ix < %(out_ix_chan_dim)) ? in[out_base_ix + in_off] : 0.0f;
     
     if( in_chan_ix >= hls ) {
-      uint32_t const out_chan_ix = in_chan_ix - hls;
+      int32_t const out_chan_ix = in_chan_ix - hls;
       float ls_sum = 0.0f;
-      for( uint32_t i = 0; i != %(local_size); ++i ) { ls_sum += ls_buf[i]*ls_buf[i]; }
+      for( int32_t i = 0; i != %(local_size); ++i ) { ls_sum += ls_buf[i]*ls_buf[i]; }
 
       float const scale = powf( (%(k) + %(alpha)*ls_sum/%(local_size)), -%(beta) );
       
