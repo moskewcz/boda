a973fa624f5d590f40f9f4861437adb06fd1850b
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-06-23 11:30:25 -0700

    stats uint32_t > int32_t fixes

--- test/rtc/stats.cu
+++ test/rtc/stats.cu
@@ -1,6 +1,6 @@
-extern "C"  __global__ void %(cu_func_name)( %(params), uint32_t const in_sz, uint32_t const primary_in ) {
-  uint32_t const tbp = 256;
-  uint32_t const tid = threadIdx.x;
-  uint32_t const ix = blockDim.x * blockIdx.x + tid;
+extern "C"  __global__ void %(cu_func_name)( %(params), int32_t const in_sz, int32_t const primary_in ) {
+  int32_t const tbp = 256;
+  int32_t const tid = threadIdx.x;
+  int32_t const ix = blockDim.x * blockIdx.x + tid;
   %(body)
 }
