3881b5119c5b0574e4961d24b90b12b2f1248b99
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-08-12 13:47:14 -0700

    rtc: refactor xpose_filts and add #if 0'd debugging set-to-val code

--- test/rtc/xpose_filts.cu
+++ test/rtc/xpose_filts.cu
@@ -2,7 +2,7 @@ extern "C"  __global__ void %(cu_func_name)( float const * const in, float * con
   int32_t const filts_ix = blockDim.x * blockIdx.x + threadIdx.x;
   if( filts_ix >= %(filts_ix_sz) ) { return; }
   int32_t const fioc = %(filts_ix_out_chan);
-  
+  float val = 0.0f;  
   int32_t const filts_xp_ix  = 
     %(fioc_out_chan_blk)*%(filts_xp_ix_out_chan_blk_sz) +
     %(fioc_out_chan_reg)*%(filts_xp_ix_out_chan_reg_sz) +
@@ -10,6 +10,16 @@ extern "C"  __global__ void %(cu_func_name)( float const * const in, float * con
     %(filts_ix_in_chan)*%(filts_xp_ix_in_chan_sz) +
     %(filts_ix_y)*%(filts_xp_ix_y_sz) +
     %(filts_ix_x)*%(filts_xp_ix_x_sz);
-  out[filts_xp_ix] = in[filts_ix];
+#if 1
+  val = in[filts_ix];
+#else
+  if( %(filts_ix_in_chan) == 0 ) {
+//    if( (%(filts_ix_x) == 5) && (%(filts_ix_y) == 5) ) 
+    {
+      val = %(filts_ix_x)*100 + %(filts_ix_y);
+    }
+  }
+#endif
+  out[filts_xp_ix] = val;
 }
 
