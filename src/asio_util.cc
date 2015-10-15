cd54d18449ccf08119c28f26a638caf0660aa71c
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2015-10-14 17:43:05 -0700

    asio_util: allow for 'raw' worker creation when ASIO based comm isn't needed
    the only difference is that the raw socket fd is returned, rather than
    being wrapped in a boost ASIO gizmo.

--- src/asio_util.cc
+++ src/asio_util.cc
@@ -7,8 +7,7 @@ namespace boda
 
   string get_boda_shm_filename( void ) { return strprintf( "/boda-rev-%s-pid-%s-top.shm", get_build_rev(), 
 							   str(getpid()).c_str() ); }
-
-  void create_boda_worker( io_service_t & io, p_asio_alss_t & alss, vect_string const & args ) {
+  int create_boda_worker( vect_string const & args ) {
     int sp_fds[2];
     neg_one_fail( socketpair( AF_LOCAL, SOCK_STREAM, 0, sp_fds ), "socketpair" );
     set_fd_cloexec( sp_fds[0], 0 ); // we want the parent fd closed in our child
@@ -16,8 +15,12 @@ namespace boda
     fin_args.push_back( strprintf("--boda-parent-socket-fd=%s",str(sp_fds[1]).c_str() ) );
     fork_and_exec_self( fin_args );
     neg_one_fail( close( sp_fds[1] ), "close" ); // in the parent, we close the socket child will use
-    alss.reset( new asio_alss_t(io)  );
-    alss->assign( stream_protocol(), sp_fds[0] );
+    return sp_fds[0];
+  }
+  void create_boda_worker( io_service_t & io, p_asio_alss_t & alss, vect_string const & args ) {
+    int const fd = create_boda_worker( args );
+    alss.reset( new asio_alss_t(io) );
+    alss->assign( stream_protocol(), fd );
   }
 
 
