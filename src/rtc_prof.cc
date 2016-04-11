da7b8cd75e4103e6343860c7eda0188aca9596f0
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2016-04-11 16:55:51 -0700

    rtc_prof: make more things be per-func
    working towards everything being per-func to avoid memory issues with
    large numbers of functions to profile. even that probably won't be
    enough to enable really large sweeps, but it's progress (hopefully).

--- src/rtc_prof.cc
+++ src/rtc_prof.cc
@@ -39,6 +39,7 @@ namespace boda
 
     string gen_func( rtc_func_sig_t const & rfs );
     void run_rfc( rcg_func_call_t & rfc );
+    void run_call( string const & func_name, p_rtc_call_gen_t const & rcg );
     void run_calls( void );
   };
  
@@ -47,11 +48,14 @@ namespace boda
     return codegen.gen_func( ccc.get(), rfs ); 
   }
 
+  p_ofstream out;
   void rtc_prof_t::main( nesi_init_arg_t * nia ) {
+    out = ofs_open( per_call_fn );
     rtc->init();
+    bool const enable_prof = 0;
+    if( enable_prof ) { rtc->profile_start(); }
     if( eat_megs ) { rtc->create_var_with_dims_floats( "MEMEATER", dims_t{ {1024,1024,eat_megs}, {"a","b","M"}, 1 } ); }
     codegen.read_rtc_func_sigs( rtc_func_sigs_fn );
-    uint32_t call_ix = 0;
     for( rtc_func_names_map_t::iterator i = codegen.rtc_func_names_map.begin(); i != codegen.rtc_func_names_map.end(); ++i ) { 
       p_rtc_call_gen_t const &rcg = i->second;
       if( !rcg->blks ) { 
@@ -62,71 +66,42 @@ namespace boda
 	printf( "skipping %s; u32 arg handling todo\n", str(rcg->type).c_str() );
 	continue; 
       }
-      map_str_str arg_map;
-      for( vect_arg_decl_t::const_iterator i = rcg->flat_arg_decls.begin(); i != rcg->flat_arg_decls.end(); ++i ) {
-	if( i->io_type == "REF" ) { continue; }
-	dims_t const & func_dims = rcg->get_arg_dims_by_name( i->vn );
-	if( func_dims == dims_t() ) { continue; } // NULL case -- ignore
-	string const vn = "call_"+str(call_ix)+"__"+i->vn;
-	//printf( "alloc: i->vn=%s vn=%s func_dims=%s\n", str(i->vn).c_str(), str(vn).c_str(), str(func_dims).c_str() );
-	//rtc->create_var_with_dims_floats( vn, func_dims );
-	must_insert( arg_map, i->vn, vn );
-      }
-      calls.push_back( rcg_func_call_t{ i->first, "tag", arg_map } );
-      ++call_ix;
+      run_call( i->first, rcg );
     }
-
-#if 0
-    if( enable_bwai_test ) { // test bwai gen
-      assert_st(0);
-      rtc->create_var_with_dims_floats( "a", dims_t{ {1000,1024}, {"M","K"}, 1 } );
-      rtc->create_var_with_dims_floats( "b", dims_t{ {1000,1024}, {"N","K"}, 1 } );
-      rtc->create_var_with_dims_floats( "c", dims_t{ {1000,1000}, {"M","N"}, 1 } );
-      map_str_dims_t bwai_dims_vals;
-      bwai_dims_vals["work"] = dims_t{ {10,10,10,10,32,10,10}, {"Mg","Ng","Mb","Nb","Kb","Mt","Nt"}, 1 };
-      gen_call( "bwai", map_str_str(), "bwai_sgemm", {"a","b","c"}, bwai_dims_vals, 0 );
-    }
-#endif
-    
-
+    if( enable_prof ) { rtc->profile_stop(); }
     rtc->finish_and_sync();
-    run_calls();
   }
 
   void rtc_prof_t::run_rfc( rcg_func_call_t & rfc ) { codegen.run_rfc( rtc, show_rtc_calls, rfc, 0 );  }
 
-  void rtc_prof_t::run_calls( void ) {
+  void rtc_prof_t::run_call( string const & func_name, p_rtc_call_gen_t const & rcg ) {
     timer_t t("rtc_prof_t::run_fwd");
-    bool const enable_prof = 0;
-    if( enable_prof ) { rtc->profile_start(); }
-    for( vect_rcg_func_call_t::iterator i = calls.begin(); i != calls.end(); ++i ) { 
-      printf( "run: i->rtc_func_name=%s\n", str(i->rtc_func_name).c_str() );
-      p_rtc_call_gen_t const & rcg = must_find( codegen.rtc_func_names_map, i->rtc_func_name );
-      rtc->compile( rcg->rtc_prog_str, show_compile_log, enable_lineinfo, {i->rtc_func_name}, show_func_attrs );
-      for( map_str_str::const_iterator j = i->arg_map.begin(); j != i->arg_map.end(); ++j ) {
-	rtc->create_var_with_dims_floats( j->second, must_find( rcg->dims_vals, j->first ) );
-      }
-      run_rfc( *i ); 
-      for( map_str_str::const_iterator j = i->arg_map.begin(); j != i->arg_map.end(); ++j ) {
-	rtc->release_var( j->second );
-      }
-      rtc->release_all_funcs();
+    map_str_str arg_map;
+    for( vect_arg_decl_t::const_iterator i = rcg->flat_arg_decls.begin(); i != rcg->flat_arg_decls.end(); ++i ) {
+      if( i->io_type == "REF" ) { continue; }
+      dims_t const & func_dims = rcg->get_arg_dims_by_name( i->vn );
+      if( func_dims == dims_t() ) { continue; } // NULL case -- ignore
+      must_insert( arg_map, i->vn, i->vn );
     }
-    rtc->finish_and_sync();
-    float const compute_dur = calls.empty() ? 0.0f : rtc->get_dur( calls.front().call_id, calls.back().call_id );
-    if( enable_prof ) { rtc->profile_stop(); }
-    if( !per_call_fn.in.empty() ) {
-      p_ofstream out = ofs_open( per_call_fn );
-      (*out) << strprintf("net.args.runtime=%s\n", str(compute_dur/1000.0).c_str() );
-      for( vect_rcg_func_call_t::iterator i = calls.begin(); i != calls.end(); ++i ) {
-	rcg_func_call_t & rfc = *i;
-	if( rfc.call_tag.empty() ) { continue; }
-	float const rfc_dur = rtc->get_dur( rfc.call_id, rfc.call_id );
-	(*out) << strprintf( "per_layer_time['%s']=per_layer_time.get('%s',0.0) + %s # %s \n", 
-			     str(rfc.call_tag).c_str(), str(rfc.call_tag).c_str(), str(rfc_dur/1000.0).c_str(), rfc.rtc_func_name.c_str() );
-      }
+    printf( "run: i->rtc_func_name=%s\n", str(func_name).c_str() );
+    rtc->compile( rcg->rtc_prog_str, show_compile_log, enable_lineinfo, {func_name}, show_func_attrs );
+    for( map_str_str::const_iterator j = arg_map.begin(); j != arg_map.end(); ++j ) {
+      rtc->create_var_with_dims_floats( j->second, must_find( rcg->dims_vals, j->first ) );
     }
+    rcg_func_call_t rfc{ func_name, "tag", arg_map };
+    run_rfc( rfc ); 
+    for( map_str_str::const_iterator j = arg_map.begin(); j != arg_map.end(); ++j ) {
+      rtc->release_var( j->second );
+    }
+    rtc->release_all_funcs();
+    // get call duration
+    //if( rfc.call_tag.empty() ) { release; return; } // FIXME: possible here? 
+    float const rfc_dur = rtc->get_dur( rfc.call_id, rfc.call_id );
+    (*out) << strprintf( "per_layer_time['%s']=per_layer_time.get('%s',0.0) + %s # %s \n", 
+			 str(rfc.call_tag).c_str(), str(rfc.call_tag).c_str(), str(rfc_dur/1000.0).c_str(), rfc.rtc_func_name.c_str() );
+    rtc->release_per_call_id_data();
   }
+
 #include"gen/rtc_prof.cc.nesi_gen.cc"
 
 }
