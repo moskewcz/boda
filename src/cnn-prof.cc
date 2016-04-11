e69891b2e20f267e17a82f2800a12fc14cb579c7
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2016-04-11 15:03:16 -0700

    cnn-prof: write latex table rows to file, not stdout

--- src/cnn-prof.cc
+++ src/cnn-prof.cc
@@ -58,18 +58,19 @@ namespace boda
   {
     virtual cinfo_t const * get_cinfo( void ) const; // required declaration for NESI support
     filename_t cnn_func_sigs_fn; //NESI(default="%(boda_test_dir)/cnn_func_sigs_tiny.txt",help="file to read cnn ops from")
+    filename_t op_info_tab_fn; //NESI(default="%(boda_output_dir)/op-info-tab.tex",help="file to write op info latex rows to")
     virtual void main( nesi_init_arg_t * nia );
   };
 
   void cnn_op_info_t::main( nesi_init_arg_t * nia ) {
     vect_p_conv_op_t sigs;
     p_vect_string in_lines = readlines_fn( cnn_func_sigs_fn );
-
+    p_ofstream oit_out = ofs_open( op_info_tab_fn );
     for( vect_string::const_iterator i = in_lines->begin(); i != in_lines->end(); ++i ) {
       p_conv_op_base_t op = make_p_conv_op_base_t_init_and_check_unused_from_lexp( parse_lexp( *i ), 0 );
       op->set_and_check_coi();
       string const s = conv_op_info_as_latex_tab_row( op );
-      printstr( s + "\n" );
+      (*oit_out) << s << "\n";
     }
   }
 
