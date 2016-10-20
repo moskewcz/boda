90559dfc0f373d3cd5ecb061c30646d1cd58889a
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2016-10-20 08:12:02 -0700

    regenerate test/conv-ops-1-5-20-nin-alex-gn.txt in current format
    note that we didn't manually remove the out_chan=4096 cases like we
    did before yet, so that may happen in a follow-on commit.

--- pysrc/cnn-prof-op-sigs-gen.py
+++ pysrc/cnn-prof-op-sigs-gen.py
@@ -7,4 +7,13 @@ for model_dir,net_name in runs:
     for img in imgs:
         os.system( "boda run_cnet --model-name=%s --in_dims='(img=%s)' --conv_fwd='(mode=rtc,write_op_sigs=1)'" %(model_dir,img) )
 
+# just run with no args, will remove and then create op_sigs_full.txt:
+# moskewcz@maaya:~/git_work/boda/run/tr3$ python ../../pysrc/cnn-prof-op-sigs-gen.py 
+# .....
 
+# moskewcz@maaya:~/git_work/boda/run/tr3$ wc op_sigs_full.txt 
+#    429    429 104011 op_sigs_full.txt
+
+#  notes: to filter for just Convolutions:
+# moskewcz@maaya:~/git_work/boda/run/tr3$ grep type=Convolution op_sigs_full.txt  | wc
+#    204     204   64918
