2d3299a47ae70092eac45d602454f5e1bb641cdc
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2016-10-28 12:31:47 -0700

    rtc_func_gen: reduce func gc threshold from 1000 to 256
    for debugging SD820 out-out-resources error. unclear if it's related,
    but there were 700+ functions resident at the time it failed.

--- src/rtc_func_gen.cc
+++ src/rtc_func_gen.cc
@@ -671,7 +671,7 @@ namespace boda
     }
   }
   
-  void rtc_codegen_t::gc_clear( void ) { if( rtc_func_sigs_map.size() > 1000 ) { clear(); } }
+  void rtc_codegen_t::gc_clear( void ) { if( rtc_func_sigs_map.size() > 256 ) { clear(); } }
 
   void rtc_codegen_t::read_rtc_func_sigs( filename_t const & rtc_func_sigs_fn ) {
     p_vect_string in_lines = readlines_fn( rtc_func_sigs_fn );
