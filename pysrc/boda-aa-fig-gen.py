d432db7e914e02ec75f75da2f0d7e43433a1ba4e
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2016-11-03 12:37:24 -0700

    boda-aa-fig-gen: add final .pdf filenames to per-figure comments

--- pysrc/boda-aa-fig-gen.py
+++ pysrc/boda-aa-fig-gen.py
@@ -24,24 +24,24 @@ class figgen_t( object ):
         fmt_data = { "wis" : "--wisdom-in-fn=" + args.wis_fn, 
                      "cudnn_ref" : "--ref-tune='(use_be=nvrtc,use_culibs=1)'",
                      "s_img" : "--s-img=5" }
-        # generate ocl/nvrtc comparison figure
+        # generate ocl/nvrtc comparison figure: titan-ocl-vs-nvrtc.pdf
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s --s-plat='ocl.*TITAN' --csv-out-fn=out-tit-ocl-5-pom.csv --csv-res-tag='-ocl' --show-ref=0 --show-aom=0",
             "boda wis-ana %(wis)s %(s_img)s %(cudnn_ref)s --s-plat='nvrtc.*TITAN' --csv-out-fn=out-tit-nvrtc-5-pom.csv --csv-res-tag='-nvrtc' --show-ref=0 --show-aom=0",
             "python ../../pysrc/wis-plot.py out-tit-ocl-5-pom.csv out-tit-nvrtc-5-pom.csv --out-fn=titan-ocl-vs-nvrtc --out-fmt=pdf --title='OpenCL vs CUDA(nvrtc) Speed on NVIDIA Titan-X(Maxwell)'",
         ]
         run_cmds( cmds, fmt_data )
-        # generate AMD tuning figure
+        # generate AMD tuning figure: fiji-tune.pdf
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s --s-plat='Fiji' --csv-out-fn=out-fiji.csv --show-ref=0",
             "python ../../pysrc/wis-plot.py out-fiji.csv --out-fn=fiji-tune --out-fmt=pdf --title='Tuned and Autotuned Speed on AMD R9-Nano(Fiji)'" ]
         run_cmds( cmds, fmt_data )
-        # generate NVIDIA tuning figure
+        # generate NVIDIA tuning figure: titan-tune.pdf
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s %(cudnn_ref)s --s-plat='nvrtc.*TITAN' --csv-out-fn=out-titan.csv --show-ref=1",
             "python ../../pysrc/wis-plot.py out-titan.csv --out-fn=titan-tune --out-fmt=pdf --title='Tuned, Autotuned, and Reference(cuDNNv5) Speed on NVIDIA Titan-X(Maxwell)'" ]
         run_cmds( cmds, fmt_data )
-        # generate all-plats figure
+        # generate all-plats figure: all-plats.pdf
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s %(cudnn_ref)s --s-plat='nvrtc.*TITAN' --csv-out-fn=all-plats-titan.csv --show-ref=0 --show-aom=0 --csv-res-tag='-titan'",
             "boda wis-ana %(wis)s %(s_img)s --s-plat='Fiji' --csv-out-fn=all-plats-fiji.csv --show-ref=0 --show-aom=0 --csv-res-tag='-R9'",
