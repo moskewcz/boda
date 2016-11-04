72b8b0f73860450c5446cc226d7de2263de621f6
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2016-11-03 20:48:08 -0700

    boda-aa-fig-gen: add output-directory option

--- pysrc/boda-aa-fig-gen.py
+++ pysrc/boda-aa-fig-gen.py
@@ -10,6 +10,7 @@
 
 
 import os
+ospj = os.path.join
 
 def run_cmds( cmds, fmt_data ):
     for cmd in cmds:
@@ -24,29 +25,29 @@ class figgen_t( object ):
         fmt_data = { "wis" : "--wisdom-in-fn=" + args.wis_fn, 
                      "cudnn_ref" : "--ref-tune='(use_be=nvrtc,use_culibs=1)'",
                      "s_img" : "--s-img=5" }
-        # generate ocl/nvrtc comparison figure: titan-ocl-vs-nvrtc.pdf
+        fmt_data["out_fn"] = ospj(self.args.out_dir,"titan-ocl-vs-nvrtc") # generate ocl/nvrtc comparison figure
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s --s-plat='ocl.*TITAN' --csv-out-fn=out-tit-ocl-5-pom.csv --csv-res-tag='-ocl' --show-ref=0 --show-aom=0",
             "boda wis-ana %(wis)s %(s_img)s %(cudnn_ref)s --s-plat='nvrtc.*TITAN' --csv-out-fn=out-tit-nvrtc-5-pom.csv --csv-res-tag='-nvrtc' --show-ref=0 --show-aom=0",
-            "python ../../pysrc/wis-plot.py out-tit-ocl-5-pom.csv out-tit-nvrtc-5-pom.csv --out-fn=titan-ocl-vs-nvrtc --out-fmt=pdf --title='OpenCL vs CUDA(nvrtc) Speed on NVIDIA Titan-X(Maxwell)'",
+            "python ../../pysrc/wis-plot.py out-tit-ocl-5-pom.csv out-tit-nvrtc-5-pom.csv --out-fn=%(out_fn)s --out-fmt=pdf --title='OpenCL vs CUDA(nvrtc) Speed on NVIDIA Titan-X(Maxwell)'",
         ]
         run_cmds( cmds, fmt_data )
-        # generate AMD tuning figure: fiji-tune.pdf
+        fmt_data["out_fn"] = ospj(self.args.out_dir,"fiji-tune") # generate AMD tuning figure
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s --s-plat='Fiji' --csv-out-fn=out-fiji.csv --show-ref=0",
-            "python ../../pysrc/wis-plot.py out-fiji.csv --out-fn=fiji-tune --out-fmt=pdf --title='Tuned and Autotuned Speed on AMD R9-Nano(Fiji)'" ]
+            "python ../../pysrc/wis-plot.py out-fiji.csv --out-fn=%(out_fn)s --out-fmt=pdf --title='Tuned and Autotuned Speed on AMD R9-Nano(Fiji)'" ]
         run_cmds( cmds, fmt_data )
-        # generate NVIDIA tuning figure: titan-tune.pdf
+        fmt_data["out_fn"] = ospj(self.args.out_dir, "titan-tune") # generate NVIDIA tuning figure
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s %(cudnn_ref)s --s-plat='nvrtc.*TITAN' --csv-out-fn=out-titan.csv --show-ref=1",
-            "python ../../pysrc/wis-plot.py out-titan.csv --out-fn=titan-tune --out-fmt=pdf --title='Tuned, Autotuned, and Reference(cuDNNv5) Speed on NVIDIA Titan-X(Maxwell)'" ]
+            "python ../../pysrc/wis-plot.py out-titan.csv --out-fn=%(out_fn)s --out-fmt=pdf --title='Tuned, Autotuned, and Reference(cuDNNv5) Speed on NVIDIA Titan-X(Maxwell)'" ]
         run_cmds( cmds, fmt_data )
-        # generate all-plats figure: all-plats.pdf
+        fmt_data["out_fn"] = ospj(self.args.out_dir,"all-plats") # generate all-plats figure
         cmds = [
             "boda wis-ana %(wis)s %(s_img)s %(cudnn_ref)s --s-plat='nvrtc.*TITAN' --csv-out-fn=all-plats-titan.csv --show-ref=0 --show-aom=0 --csv-res-tag='-titan'",
             "boda wis-ana %(wis)s %(s_img)s --s-plat='Fiji' --csv-out-fn=all-plats-fiji.csv --show-ref=0 --show-aom=0 --csv-res-tag='-R9'",
             "boda wis-ana %(wis)s %(s_img)s --s-plat='Adreno' --csv-out-fn=all-plats-SD820.csv --show-ref=0 --show-aom=0 --csv-res-tag='-SD820'",
-            "python ../../pysrc/wis-plot.py all-plats-titan.csv all-plats-fiji.csv all-plats-SD820.csv --out-fn=all-plats --out-fmt=pdf --title='Autotuned Speed on NVIDIA Titan-X, AMD R9-Nano, and Qualcomm Snapdragon 820'" ]
+            "python ../../pysrc/wis-plot.py all-plats-titan.csv all-plats-fiji.csv all-plats-SD820.csv --out-fn=%(out_fn)s --out-fmt=pdf --title='Autotuned Speed on NVIDIA Titan-X, AMD R9-Nano, and Qualcomm Snapdragon 820'" ]
         run_cmds( cmds, fmt_data )
         
 
@@ -57,6 +58,7 @@ class figgen_t( object ):
 import argparse
 parser = argparse.ArgumentParser(description='generate list of figures for boda-aa paper.')
 parser.add_argument('--wis-fn', metavar="FN", type=str, default="../../test/wisdom-merged.wis", help="filename of results database" )
+parser.add_argument('--out-dir', metavar="FN", type=str, default="../../../boda-autotune-amd/figs", help="filename of results database" )
 args = parser.parse_args()
 
 figgen_t( args )
