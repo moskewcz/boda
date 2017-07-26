faa76f2dc62f5ee6405d02c60dd7b0ba602627ed
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2017-07-26 14:43:16 -0700

    remove empty/unused raw-vid-io.H

--- src/raw-vid-io.cc
+++ src/raw-vid-io.cc
@@ -1,6 +1,5 @@
 // Copyright (c) 2015, Matthew W. Moskewicz <moskewcz@alumni.princeton.edu>; part of Boda framework; see LICENSE
 #include"boda_tu_base.H"
-#include"raw-vid-io.H"
 #include"geom_prim.H"
 #include"img_io.H"
 #include"has_main.H"
