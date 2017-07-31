fd06c29c4ea386bb3e7fa534d713200ab211d797
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2017-07-31 16:03:15 -0700

    split data_stream_base_t into data_stream_{qt,text_dumpvideo}_t

--- src/data-stream.cc
+++ src/data-stream.cc
@@ -34,102 +34,60 @@ namespace boda
       for( uint32_t i = 0; i != skip_blocks; ++i ) { src->read_next_block(); } // skip blocks if requested
       return ret;
     }
-    
-    // init/setup
+
     virtual void data_stream_init( nesi_init_arg_t * nia ) {
       src->data_stream_init( nia );
       printf( "data_stream_init(): mode=%s start_block=%s skip_blocks=%s num_to_read=%s\n",
               str(mode).c_str(), str(start_block).c_str(), str(skip_blocks).c_str(), str(num_to_read).c_str() );
       tot_num_read = 0;
-      // skip to start block
-      for( uint32_t i = 0; i != start_block; ++i ) { src->read_next_block(); }
+      for( uint32_t i = 0; i != start_block; ++i ) { src->read_next_block(); } // skip to start block
     }
     
-    void main( nesi_init_arg_t * nia ) { 
-      data_stream_init( nia );
-      while( read_next_block().d.get() ) { }
-    }
   };
-
-  struct data_stream_base_t : virtual public nesi, public data_stream_t // NESI(help="parse data stream (dumpvideo/qt) into data blocks",
-                             // bases=["data_stream_t"], type_id="base")
+  
+  struct data_stream_qt_t : virtual public nesi, public data_stream_t // NESI(help="parse qt-style-serialized data stream into data blocks",
+                            // bases=["data_stream_t"], type_id="qt")
   {
     virtual cinfo_t const * get_cinfo( void ) const; // required declaration for NESI support
     uint32_t verbose; //NESI(default="0",help="verbosity level (max 99)")
     filename_t fn; //NESI(default="vid.raw",help="input raw video filename")
-    string read_mode; //NESI(default="dumpvideo",help="file reading mode: dumpvideo=mplayer video dump format; qt=qt-style binary encapsulation")
-
-    uint32_t text_tsfix; //NESI(default=0,help="text time-stamp-field-index; when read_mode==text, use the N'th field as a decimal timestamp in seconds (with fractional part).")
-
     uint64_t tot_num_read; // num blocks read so far
-
     mapped_file_stream_reader mfsr;
-    
-
-    // set timestamp from field of text line stored in block
-    void set_timestamp_from_text_line( data_block_t & v ) {
-      string line( v.d.get(), v.d.get()+v.sz );
-      vect_string parts = split( line, ' ' );
-      if( !( text_tsfix < parts.size() ) ) {
-        rt_err( strprintf( "can't parse timestamp from text_tsfix=%s; line had parts.size()=%s; full line=%s\n", str(text_tsfix).c_str(), str(parts.size()).c_str(), str(line).c_str() ) );
-      }
-      //if( verbose ) { printf( "parts[text_tsfix]=%s\n", str(parts[text_tsfix]).c_str() ); }
-      double const ts_d_ns = lc_str_d( parts[text_tsfix] ) * 1e9;
-      v.timestamp_ns = lround(ts_d_ns);
-    }
-
-    // block-level reading
     
     virtual string get_pos_info_str( void ) { return strprintf( "pos=%s tot_num_read=%s", str(mfsr.pos).c_str(), str(tot_num_read).c_str() ); }
     
     virtual data_block_t read_next_block( void ) {
       data_block_t ret;
-      if( 0 ) {}
-      else if( read_mode == "dumpvideo" ) {
-        uint32_t block_sz;
-        if( !mfsr.can_read( sizeof( block_sz ) ) ) { return ret; } // not enough bytes left for another block
-        mfsr.read_val( block_sz );
-        ret = mfsr.consume_borrowed_block( block_sz ); // note: timestamp not set here
-      } else if( read_mode == "qt" ) {
-        if( mfsr.pos == timestamp_off ) {
+      if( mfsr.pos == timestamp_off ) {
 #if 0
-          printf( "at timestamp_off: pos=%s\n", str(mfsr.pos).c_str() );
-          uint8_t ch;
-          while( mfsr.can_read( sizeof(ch) ) ) {
-            mfsr.read_val(ch);
-            printf( "ch=%s ch=%s\n", str(uint32_t(ch)).c_str(), str(ch).c_str() );
-          }
-#endif     
-          return ret;
+        printf( "at timestamp_off: pos=%s\n", str(mfsr.pos).c_str() );
+        uint8_t ch;
+        while( mfsr.can_read( sizeof(ch) ) ) {
+          mfsr.read_val(ch);
+          printf( "ch=%s ch=%s\n", str(uint32_t(ch)).c_str(), str(ch).c_str() );
         }
-        if( !mfsr.can_read( sizeof( ret.timestamp_ns ) ) ) { return ret; } // not enough bytes left for another block
-        mfsr.read_val( ret.timestamp_ns );
-        if( !mfsr.can_read( sizeof( uint32_t ) ) ) { rt_err( "qt stream: read timestamp, but not enough data left to read payload size" ); }
-        mfsr.read_val( ret );
-      } else if( read_mode == "text" ) {
-        if( !mfsr.can_read( 1 ) ) { return ret; } // not enough bytes left for another block
-        mfsr.read_line_as_block( ret );
-        set_timestamp_from_text_line( ret );
-      } else { rt_err( "unknown read_mode: " + read_mode ); }
-      
+#endif     
+        return ret;
+      }
+      if( !mfsr.can_read( sizeof( ret.timestamp_ns ) ) ) { return ret; } // not enough bytes left for another block
+      mfsr.read_val( ret.timestamp_ns );
+      if( !mfsr.can_read( sizeof( uint32_t ) ) ) { rt_err( "qt stream: read timestamp, but not enough data left to read payload size" ); }
+      mfsr.read_val( ret );
+
       ++tot_num_read;
-      if( verbose ) { printf( "%s ret.sz=%s ret.timestamp_ns=%s\n", read_mode.c_str(), str(ret.sz).c_str(), str(ret.timestamp_ns).c_str() ); }
+      if( verbose ) { printf( "ret.sz=%s ret.timestamp_ns=%s\n", str(ret.sz).c_str(), str(ret.timestamp_ns).c_str() ); }
       return ret;
     }
 
-    // init/setup
-    
-    void data_stream_init_dumpvideo( void ) {
-      mfsr.need_endian_reverse = 0;
-    }
-
-    // for qt stream
+    // qt stream info/state
     uint64_t timestamp_off;
     uint64_t chunk_off;
 
-    void data_stream_init_qt( void ) {
+    virtual void data_stream_init( nesi_init_arg_t * nia ) {
+      printf( "data_stream_init(): mode=%s fn.exp=%s\n", str(mode).c_str(), str(fn.exp).c_str() );
+      mfsr.init( fn );
+      tot_num_read = 0;
       mfsr.need_endian_reverse = 1; // assume stream is big endian, and native is little endian. could check this ...
-
       uint32_t ver;
       mfsr.read_val( ver );
       string tag;
@@ -148,25 +106,85 @@ namespace boda
                 str((mfsr.size() - timestamp_off)).c_str() );
       }
     }
+  };
 
-    void data_stream_init_text( void ) {
+  struct data_stream_dumpvideo_t : virtual public nesi, public data_stream_t // NESI(help="parse dumpvideo data stream into data blocks",
+                                   // bases=["data_stream_t"], type_id="dumpvideo")
+  {
+    virtual cinfo_t const * get_cinfo( void ) const; // required declaration for NESI support
+    uint32_t verbose; //NESI(default="0",help="verbosity level (max 99)")
+    filename_t fn; //NESI(default="vid.raw",help="input raw video filename")
+
+    // internal state
+    uint64_t tot_num_read; // num blocks read so far
+    mapped_file_stream_reader mfsr;
+    
+    virtual string get_pos_info_str( void ) { return strprintf( "pos=%s tot_num_read=%s", str(mfsr.pos).c_str(), str(tot_num_read).c_str() ); }
+    
+    virtual data_block_t read_next_block( void ) {
+      data_block_t ret;
+      uint32_t block_sz;
+      if( !mfsr.can_read( sizeof( block_sz ) ) ) { return ret; } // not enough bytes left for another block
+      mfsr.read_val( block_sz );
+      ret = mfsr.consume_borrowed_block( block_sz ); // note: timestamp not set here
+      if( verbose ) { printf( "ret.sz=%s ret.timestamp_ns=%s\n", str(ret.sz).c_str(), str(ret.timestamp_ns).c_str() ); }
+      return ret;
+    }
+
+    virtual void data_stream_init( nesi_init_arg_t * nia ) {
+      printf( "data_stream_init(): mode=%s fn.exp=%s\n", str(mode).c_str(), str(fn.exp).c_str() );
+      mfsr.init( fn );
+      tot_num_read = 0;
+    }
+  };
+
+  struct data_stream_text_t : virtual public nesi, public data_stream_t // NESI(help="parse data stream (dumpvideo/qt) into data blocks",
+                             // bases=["data_stream_t"], type_id="text")
+  {
+    virtual cinfo_t const * get_cinfo( void ) const; // required declaration for NESI support
+    uint32_t verbose; //NESI(default="0",help="verbosity level (max 99)")
+    filename_t fn; //NESI(default="vid.raw",help="input raw video filename")
+    uint32_t text_tsfix; //NESI(default=0,help="text time-stamp-field-index: use the N'th field as a decimal timestamp in seconds (with fractional part).")
+
+    // internal state
+    mapped_file_stream_reader mfsr;
+    uint64_t tot_num_read; // num blocks read so far
+    
+    // set timestamp from field of text line stored in block
+    void set_timestamp_from_text_line( data_block_t & v ) {
+      string line( v.d.get(), v.d.get()+v.sz );
+      vect_string parts = split( line, ' ' );
+      if( !( text_tsfix < parts.size() ) ) {
+        rt_err( strprintf( "can't parse timestamp from text_tsfix=%s; line had parts.size()=%s; full line=%s\n", str(text_tsfix).c_str(), str(parts.size()).c_str(), str(line).c_str() ) );
+      }
+      //if( verbose ) { printf( "parts[text_tsfix]=%s\n", str(parts[text_tsfix]).c_str() ); }
+      double const ts_d_ns = lc_str_d( parts[text_tsfix] ) * 1e9;
+      v.timestamp_ns = lround(ts_d_ns);
+    }
+
+    virtual string get_pos_info_str( void ) { return strprintf( "pos=%s tot_num_read=%s", str(mfsr.pos).c_str(), str(tot_num_read).c_str() ); }
+    
+    virtual data_block_t read_next_block( void ) {
+      data_block_t ret;
+      if( !mfsr.can_read( 1 ) ) { return ret; } // not enough bytes left for another block
+      mfsr.read_line_as_block( ret );
+      set_timestamp_from_text_line( ret );
+
+      ++tot_num_read;
+      if( verbose ) { printf( "ret.sz=%s ret.timestamp_ns=%s\n", str(ret.sz).c_str(), str(ret.timestamp_ns).c_str() ); }
+      return ret;
+    }
+
+    virtual void data_stream_init( nesi_init_arg_t * nia ) {
+      printf( "data_stream_init(): mode=%s fn.exp=%s\n", str(mode).c_str(), str(fn.exp).c_str() );
+      mfsr.init( fn );
+      tot_num_read = 0;
+
       data_block_t header;
       mfsr.read_line_as_block( header );
       printf( "  text stream header.sz=%s\n", str(header.sz).c_str() );
     }
-
-    virtual void data_stream_init( nesi_init_arg_t * nia ) {
-      printf( "data_stream_init(): mode=%s fn.exp=%s read_mode=%s\n", str(mode).c_str(), str(fn.exp).c_str(), str(read_mode).c_str() );
-      mfsr.init( fn );
-      tot_num_read = 0;
-      if( 0 ) {}
-      else if( read_mode == "dumpvideo" ) { data_stream_init_dumpvideo(); }
-      else if( read_mode == "qt" ) { data_stream_init_qt(); }
-      else if( read_mode == "text" ) { data_stream_init_text(); }
-      else { rt_err( "unknown read_mode: " + read_mode ); }
-    }
   };
-  struct data_stream_base_t; typedef shared_ptr< data_stream_base_t > p_data_stream_base_t; 
 
   struct block_info_t {
     uint16_t block_id;
@@ -183,7 +201,7 @@ namespace boda
   {
     virtual cinfo_t const * get_cinfo( void ) const; // required declaration for NESI support
     uint32_t verbose; //NESI(default="0",help="verbosity level (max 99)")
-    p_data_stream_base_t vps; //NESI(req=1,help="underlying velodyne packet stream")
+    p_data_stream_t vps; //NESI(req=1,help="underlying velodyne packet stream")
 
     uint32_t fbs_per_packet; //NESI(default="12",help="firing blocks per packet")
     uint32_t beams_per_fb; //NESI(default="32",help="beams per firing block")
@@ -196,7 +214,8 @@ namespace boda
     // internal state:
     uint64_t tot_num_read; // num blocks read so far
 
-    virtual string get_pos_info_str( void ) { return strprintf( "tot_num_read=%s vps->tot_num_read=%s", str(tot_num_read).c_str(), str(vps->tot_num_read).c_str() ); }
+    virtual string get_pos_info_str( void ) { return strprintf( "tot_num_read=%s vps info:%s",
+                                                                str(tot_num_read).c_str(), vps->get_pos_info_str().c_str() ); }
 
     virtual data_block_t read_next_block( void ) {
       bool packet_is_rot_start = 0;
