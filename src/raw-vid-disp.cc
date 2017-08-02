f1a0636b268fffb1f3f651d42716edaa7a5e01b2
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2017-08-02 16:23:10 -0700

    switch raw-vid-disp to use a multi-data-stream instead of a vect of (regular) data_streams
    that way, the sync wrapper can be used.

--- src/raw-vid-disp.cc
+++ src/raw-vid-disp.cc
@@ -15,20 +15,21 @@ namespace boda
                              // bases=["has_main_t"], type_id="display-raw-vid")
   {
     virtual cinfo_t const * get_cinfo( void ) const; // required declaration for NESI support
-    u32_pt_t disp_sz; //NESI(default="300 300",help="X/Y display size")
+    u32_pt_t window_sz; //NESI(default="640:480",help="X/Y window size")
+    u32_pt_t disp_sz; //NESI(default="300:300",help="X/Y per-stream-image size")
     double fps; //NESI(default=5,help="frames to (try to ) send to display per second (note: independant of display rate)")
     uint32_t auto_adv; //NESI(default=1,help="if set, slideshow mode")
     uint32_t print_timestamps; //NESI(default=0,help="if set, print per-frame timestamps")
-    vect_p_data_stream_t stream; //NESI(help="data stream to read images from")
+    p_multi_data_stream_t src; //NESI(help="multi data stream to read images from")
     vect_p_data_to_img_t data_to_img; //NESI(help="data stream to img converters (must specify same # of these as data streams)")
     disp_win_t disp_win;
     p_vect_p_img_t disp_imgs;
     p_deadline_timer_t frame_timer;
     time_duration frame_dur;
 
+    uint32_t num_srcs;
     vect_p_img_t in_imgs;
-    
-
+    vect_data_block_t src_dbs;
     
     void on_frame( error_code const & ec ) {
       if( ec ) { return; }
@@ -36,17 +37,17 @@ namespace boda
       frame_timer->expires_at( frame_timer->expires_at() + frame_dur );
       frame_timer->async_wait( bind( &display_raw_vid_t::on_frame, this, _1 ) ); 
       if( !auto_adv ) { return; }
-      assert_st( stream.size() == data_to_img.size() );
-      assert_st( stream.size() == in_imgs.size() );
+      assert_st( num_srcs == data_to_img.size() );
+      assert_st( num_srcs == in_imgs.size() );
       bool had_new_img = 0;
       if( print_timestamps ) { printf( "--- frame ---\n"); }
-      for( uint32_t i = 0; i != stream.size(); ++i ) {
-        data_block_t db = stream[i]->read_next_block();
+      src->multi_read_next_block( src_dbs );
+      for( uint32_t i = 0; i != num_srcs; ++i ) {
         if( print_timestamps ) {
-          printf( "stream[%s]: got db.timestamp_ns=%s (db.size=%s)\n",
-                  str(i).c_str(), str(db.timestamp_ns).c_str(), str(db.sz).c_str() );
+          printf( "src[%s]: got db.timestamp_ns=%s (db.size=%s)\n",
+                  str(i).c_str(), str(src_dbs[i].timestamp_ns).c_str(), str(src_dbs[i].sz).c_str() );
         }
-        p_img_t img = data_to_img[i]->data_block_to_img( db );
+        p_img_t img = data_to_img[i]->data_block_to_img( src_dbs[i] );
         if( !img ) { continue; }
         had_new_img = 1;
         p_img_t ds_img = resample_to_size( img, in_imgs[i]->sz );
@@ -64,7 +65,7 @@ namespace boda
       if( 0 ) { }
       if( !lbe.is_key ) {
         if( lbe.img_ix != uint32_t_const_max ) {
-          assert_st( lbe.img_ix < stream.size() );
+          assert_st( lbe.img_ix < num_srcs );
           data_to_img[lbe.img_ix]->set_samp_pt( lbe.xy );
           printf( "set_samp_pt(%s)\n", str( lbe.xy ).c_str() );
         }
@@ -73,9 +74,7 @@ namespace boda
       //else if( lbe.is_key && (lbe.keycode == 'a') ) { mod_adj( cur_img_ix, img_db->img_infos.size(), -1 ); auto_adv=0; }
       else if( lbe.is_key && (lbe.keycode == 'i') ) {
         auto_adv=0;
-        for( uint32_t i = 0; i != stream.size(); ++i ) {
-          printf( "stream[%s]: %s\n", str(i).c_str(), stream[i]->get_pos_info_str().c_str() );
-        }
+        printf( "src: %s\n", src->get_pos_info_str().c_str() );
       }
       else if( lbe.is_key && (lbe.keycode == 'p') ) { auto_adv ^= 1; }
       else if( lbe.is_key ) { // unknown command handlers
@@ -90,17 +89,18 @@ namespace boda
     }
 
     virtual void main( nesi_init_arg_t * nia ) {
-      if( stream.size() != data_to_img.size() ) {
-        rt_err( strprintf( "error: must specify same number of data streams and data-to-img converters, but; stream.size()=%s and data_to_img.size()=%s\n",
-                           str(stream.size()).c_str(), str(data_to_img.size()).c_str() ) );
+      num_srcs = src->multi_data_stream_init( nia );
+      if( num_srcs != data_to_img.size() ) {
+        rt_err( strprintf( "error: must specify same number of data streams and data-to-img converters, but; num_srcs=%s and data_to_img.size()=%s\n",
+                           str(num_srcs).c_str(), str(data_to_img.size()).c_str() ) );
       }
-      for( uint32_t i = 0; i != stream.size(); ++i ) {
-        stream[i]->data_stream_init( nia );
+      for( uint32_t i = 0; i != num_srcs; ++i ) {
+        //stream[i]->data_stream_init( nia );
         data_to_img[i]->data_to_img_init( nia );
         in_imgs.push_back( make_shared<img_t>() );
         in_imgs.back()->set_sz_and_alloc_pels( disp_sz );
       }
-      
+      disp_win.window_sz = window_sz;
       disp_win.disp_setup( in_imgs );
 
       io_service_t & io = get_io( &disp_win );
