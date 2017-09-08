a94a9a334eda3777833604250348d86962bf4a3f
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2017-09-08 14:38:58 -0700

    os-render: intermediate commit; broken changes to cloud shader

--- src/os-render.cc
+++ src/os-render.cc
@@ -44,15 +44,15 @@ void main(){
 #version 330 core
 
 // Input vertex data, different for all executions of this shader.
-layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 0) in float pt_dist;
 
 // Output data ; will be interpolated for each fragment.
 out vec3 fragmentColor;
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
 
-//uniform uint hbins;
-//uniform uint lasers;
+uniform uint hbins;
+uniform uint lasers;
 
 vec3 hsv2rgb(vec3 c) {
    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
@@ -62,11 +62,15 @@ vec3 hsv2rgb(vec3 c) {
 
 void main(){	
   // Output position of the vertex, in clip space : MVP * position
-  gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
-  float hue = (-1. + exp(-max(vertexPosition_modelspace[2] - 0.5, 0.) / 1.5)) * 0.7 - 0.33;
+  vec3 pos;
+  pos[0] = float(uint(gl_VertexID)%lasers);
+  pos[1] = float(uint(gl_VertexID)/lasers);
+  pos[2] = pt_dist;
+  gl_Position =  MVP * vec4(pos,1);
+  float hue = (-1. + exp(-max(pos[2] - 0.5, 0.) / 1.5)) * 0.7 - 0.33;
   fragmentColor = hsv2rgb(vec3(hue, 0.8, 1.0));
   gl_PointSize = 2.;
-  gl_Position[0] += float(gl_VertexID%10);
+
 }
 
 )xxx";
