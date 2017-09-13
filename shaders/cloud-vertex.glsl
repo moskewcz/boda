d76eeb5f4a89d0950674e6a81abbb322cb25fefc
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2017-09-13 14:32:52 -0700

    os-render: more work toward actual point-cloud data vis
    it's dangerously close to looking non-random now.

--- shaders/cloud-vertex.glsl
+++ shaders/cloud-vertex.glsl
@@ -41,9 +41,9 @@ void main(){
   uint hbin = uint(gl_VertexID) % hbins;
   //float elev_ang = radians(-24.8) + float(laser_id) * radians(0.5);     
   float elev_ang = radians(texelFetch(lut_tex, int(laser_id)*SIZEOF_LC + OFF_LC_VERT ).r);
-  float azi_ang = (float(hbin) - float(hbins)/2.0)*radians(0.172) + radians(texelFetch(lut_tex, int(laser_id)*SIZEOF_LC + OFF_LC_ROT ).r);
-  //float dist = 50.;
-  float dist = pt_dist / 500.;
+  float azi_ang = (float(hbin) - float(hbins)/2.0)*radians(0.172) - radians(texelFetch(lut_tex, int(laser_id)*SIZEOF_LC + OFF_LC_ROT ).r);
+  float dist = 50.;
+  //float dist = pt_dist / 500.;
   float sin_azi = sin(azi_ang);
   float cos_azi = cos(azi_ang);
   float sin_elev = sin(elev_ang);
