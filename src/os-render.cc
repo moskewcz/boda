bed44c4e0b799067b5249582e082ca1659934212
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2017-12-05 09:12:46 -0800

    os-render: oops, fix count for grid glDrawArrays() call.
    GL: by turns, randomly over and under forgiving of API errors.

--- src/os-render.cc
+++ src/os-render.cc
@@ -144,7 +144,7 @@ void main(){
       glEnableVertexAttribArray(0);
       glBindBuffer(GL_ARRAY_BUFFER, grid_pts_buf);
       glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, (void *) 0 ); 
-      glDrawArrays(GL_LINES, 0, grid_pts->elems_sz() );
+      glDrawArrays(GL_LINES, 0, grid_pts->elems_sz() / 3 );
       glDisableVertexAttribArray(0);
     }
 
