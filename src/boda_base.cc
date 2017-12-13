b8757539514d030dc0d8f19e15e374ee9b5cfc2b
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2017-12-13 13:43:40 -0800

    boda_base: (finally) move check_tn to .cc file, so it can use rt_err()
    note that this required explicitly instantiating nda_T for all our
    supported types in boda_base.cc, which seems okay/fine[/good?].

--- src/boda_base.cc
+++ src/boda_base.cc
@@ -73,6 +73,22 @@ namespace boda
     return "(" + ret + ")";
   }
 
+  template< typename T >
+  void nda_T<T>::check_tn( void ) const {
+    string const & type_tn = get_ndat<T>().tn; // FIXME: can the user trigger this error? use better err msg?
+    if( dims.tn != type_tn ) { rt_err( strprintf( "internal error: nda_T check_tn() failure: type_tn=%s dims.tn=%s\n", 
+                                                  str(type_tn).c_str(), str(dims.tn).c_str() ) ); }
+  }
+  
+  // explicit class declarations, to (at least) get versions of check_tn for supported cases
+  template class nda_T<half>;
+  template class nda_T<float>;
+  template class nda_T<double>;
+  template class nda_T<uint8_t>;
+  template class nda_T<uint16_t>;
+  template class nda_T<uint32_t>;
+  template class nda_T<int32_t>;
+  
   void nda_t::reshape( dims_t const & new_dims ) {
     if( !dims.valid() ) { rt_err( "nda_t::reshape(): current dims not valid" ); }
     if( !d.get() ) { rt_err( "nda_t::reshape(): d not valid (no data)" ); }
