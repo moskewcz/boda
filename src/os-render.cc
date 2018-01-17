53deff31b77dd5ff42f72a43d31fd885c34c4f80
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2018-01-17 13:14:12 -0800

    os-render: oops, fix vec3->vec4 for grid vertex shader.
    just didn't notice errors/grid-was-missing when doing test runs ...

--- src/os-render.cc
+++ src/os-render.cc
@@ -48,14 +48,14 @@ namespace boda
 layout(location = 0) in vec3 vertexPosition_modelspace;
 
 // Output data ; will be interpolated for each fragment.
-out vec3 fragmentColor;
+out vec4 fragmentColor;
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
 
 void main(){	
   // Output position of the vertex, in clip space : MVP * position
   gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
-  fragmentColor = vec3(.4,.4,.4);
+  fragmentColor = vec4(.4,.4,.4,1.0);
 }
 
 )xxx";
