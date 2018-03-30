6633d1264ae122619e1ff654fa6418100eb1d483
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2018-03-30 09:27:01 -0700

    zmq: minimal c++/smart-pointer-ification of REQ/REP samples

--- src/zmq-util.cc
+++ src/zmq-util.cc
@@ -9,6 +9,10 @@
 
 namespace boda {
 
+
+  p_void make_p_zmq_context( void ) { return p_void( zmq_ctx_new(), zmq_ctx_destroy ); }
+  p_void make_p_zmq_socket( p_void const & context, int const & type ) { return p_void( zmq_socket( context.get(), type ), zmq_close ); }
+
   struct zmq_hello_server_t : virtual public nesi, public has_main_t // NESI(
                               // help="simple ZMQ test server ",
                               // bases=["has_main_t"], type_id="zmq-hello-server")
@@ -17,17 +21,17 @@ namespace boda {
     uint32_t verbose; //NESI(default="0",help="verbosity level (max 99)")
     void main( nesi_init_arg_t * nia ) {
       //  Socket to talk to clients
-      void *context = zmq_ctx_new ();
-      void *responder = zmq_socket (context, ZMQ_REP);
-      int rc = zmq_bind (responder, "tcp://*:5555");
-      assert (rc == 0);
+      p_void context = make_p_zmq_context();
+      p_void responder = make_p_zmq_socket(context, ZMQ_REP);
+      int rc = zmq_bind(responder.get(), "tcp://*:5555");
+      assert(rc == 0);
 
       while (1) {
-        char buffer [10];
-        zmq_recv (responder, buffer, 10, 0);
-        printf ("Received Hello\n");
-        sleep (1);          //  Do some 'work'
-        zmq_send (responder, "World", 5, 0);
+        char buffer[10];
+        zmq_recv(responder.get(), buffer, 10, 0);
+        printf("Received Hello\n");
+        sleep(1);          //  Do some 'work'
+        zmq_send(responder.get(), "World", 5, 0);
       }
     }
   };
@@ -40,20 +44,18 @@ namespace boda {
     uint32_t verbose; //NESI(default="0",help="verbosity level (max 99)")
     void main( nesi_init_arg_t * nia ) {
       printf ("Connecting to hello world server…\n");
-      void *context = zmq_ctx_new ();
-      void *requester = zmq_socket (context, ZMQ_REQ);
-      zmq_connect (requester, "tcp://localhost:5555");
+      p_void context = make_p_zmq_context();
+      p_void requester = make_p_zmq_socket(context, ZMQ_REQ);
+      zmq_connect(requester.get(), "tcp://localhost:5555");
 
       int request_nbr;
-      for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-        char buffer [10];
-        printf ("Sending Hello %d…\n", request_nbr);
-        zmq_send (requester, "Hello", 5, 0);
-        zmq_recv (requester, buffer, 10, 0);
-        printf ("Received World %d\n", request_nbr);
+      for(request_nbr = 0; request_nbr != 3; request_nbr++) {
+        char buffer[10];
+        printf("Sending Hello %d…\n", request_nbr);
+        zmq_send(requester.get(), "Hello", 5, 0);
+        zmq_recv(requester.get(), buffer, 10, 0);
+        printf("Received World %d\n", request_nbr);
       }
-      zmq_close (requester);
-      zmq_ctx_destroy (context);
     }
   };
 
