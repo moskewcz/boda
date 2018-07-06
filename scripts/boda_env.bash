7c0a5869801481cf50cb83abdd2ba1c746acf850
Arya Mazaheri <aryanet@gmail.com>
2018-07-06 13:03:19 +0200

    making boda_env compatible with zsh

--- scripts/boda_env.bash
+++ scripts/boda_env.bash
@@ -3,7 +3,19 @@
 # not need to be modified, but if the 'BODA_HOME' magic below doesn't
 # work for you, you can just hard-code the boda directory instead, or
 # just put boda in your PATH however else you please.
-BODA_HOME="$( cd "$(dirname "${BASH_SOURCE}")" ; cd .. ; pwd -P )"
+if [ -n "$ZSH_VERSION" ]; then
+   # make zsh compatible with bash completion scripts
+   autoload bashcompinit
+   bashcompinit
+   SOURCE=${(%):-%N}
+elif [ -n "$BASH_VERSION" ]; then
+   SOURCE=${BASH_SOURCE}
+else
+   echo "boda_env.bash: This script is only compatible with bash and zsh."
+   exit 1
+fi
+
+BODA_HOME="$( cd "$(dirname "${SOURCE}")" ; cd .. ; pwd -P )"
 export PATH=${BODA_HOME}/lib:$PATH
 . ${BODA_HOME}/scripts/boda_completion.bash
 
